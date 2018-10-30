e2912ac2ea87ab6e0c84f518b424f1ec2cb6ab6c
aidan-cfse <aidan.jungo@cfse.ch>
2018-10-30 11:55:06 +0100

    Small modifications and add directories

--- test/TestCPACSFunctions/test_cpacsfunction.py
+++ test/TestCPACSFunctions/test_cpacsfunction.py
@@ -1,7 +1,7 @@
 """
     CEASIOMpy: Conceptual Aircraft Design Software
 
-    Developed for CFS ENGINEERING, 1015 Lausanne, Switzerland
+    Developed by CFS ENGINEERING, 1015 Lausanne, Switzerland
 
     Test all the function from 'lib/utils/cpacsfunctions.py'
 
--- test/TestMathFunctions/test_mathfunctions.py
+++ test/TestMathFunctions/test_mathfunctions.py
@@ -1,7 +1,7 @@
 """
     CEASIOMpy: Conceptual Aircraft Design Software
 
-    Developed for CFS ENGINEERING, 1015 Lausanne, Switzerland
+    Developed by CFS ENGINEERING, 1015 Lausanne, Switzerland
 
     Test functions from 'lib/utils/mathfunctions.py'
 

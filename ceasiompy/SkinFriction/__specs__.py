e0db2d77074cf2f890979d30a19513a48f0cb708
Aaron Dettmann <dettmann@kth.se>
2019-08-12 15:46:11 +0200

    Use PEP8 recommended indentation for spec files

--- ceasiompy/SkinFriction/__specs__.py
+++ ceasiompy/SkinFriction/__specs__.py
@@ -8,51 +8,51 @@ cpacs_inout = CPACSInOut()
 # ===== Input =====
 
 cpacs_inout.add_input(
-        var_name='wetted_area',
-        default_value=None,
-        unit='m^2',
-        descr='Wetted area of the aircraft (calculated by SU2)',
-        cpacs_path=CEASIOM_XPATH + '/geometry/analysis/wettedArea',
-        )
+    var_name='wetted_area',
+    default_value=None,
+    unit='m^2',
+    descr='Wetted area of the aircraft (calculated by SU2)',
+    cpacs_path=CEASIOM_XPATH + '/geometry/analysis/wettedArea',
+)
 
 cpacs_inout.add_input(
-        var_name='cruise_speed',
-        default_value=272,
-        unit='m/s',
-        descr='Aircraft cruise speed',
-        cpacs_path=CEASIOM_XPATH + '/ranges/cruiseSpeed',
-        )
+    var_name='cruise_speed',
+    default_value=272,
+    unit='m/s',
+    descr='Aircraft cruise speed',
+    cpacs_path=CEASIOM_XPATH + '/ranges/cruiseSpeed',
+)
 
 cpacs_inout.add_input(
-        var_name='cruise_alt',
-        default_value=12000,
-        unit='m',
-        descr='Aircraft cruise altitude',
-        cpacs_path=CEASIOM_XPATH + '/ranges/cruiseAltitude',
-        )
+    var_name='cruise_alt',
+    default_value=12000,
+    unit='m',
+    descr='Aircraft cruise altitude',
+    cpacs_path=CEASIOM_XPATH + '/ranges/cruiseAltitude',
+)
 
 # ===== Output =====
 
 cpacs_inout.add_output(
-        var_name='cd0',
-        default_value=None,
-        unit='1',
-        descr='Skin friction drag coefficient',
-        cpacs_path=CEASIOM_XPATH + '/aerodynamics/su2/cd0',
-        )
+    var_name='cd0',
+    default_value=None,
+    unit='1',
+    descr='Skin friction drag coefficient',
+    cpacs_path=CEASIOM_XPATH + '/aerodynamics/su2/cd0',
+)
 
 cpacs_inout.add_output(
-        var_name='wing_area',
-        default_value=None,
-        unit='m^2',
-        descr='Wing area of the main (largest) wing',
-        cpacs_path=CEASIOM_XPATH + '/geometry/analysis/wingArea',
-        )
+    var_name='wing_area',
+    default_value=None,
+    unit='m^2',
+    descr='Wing area of the main (largest) wing',
+    cpacs_path=CEASIOM_XPATH + '/geometry/analysis/wingArea',
+)
 
 cpacs_inout.add_output(
-        var_name='wing_span',
-        default_value=None,
-        unit='m',
-        descr='Wing span of the main (largest) wing',
-        cpacs_path=CEASIOM_XPATH + '/geometry/analysis/wingSpan',
-        )
+    var_name='wing_span',
+    default_value=None,
+    unit='m',
+    descr='Wing span of the main (largest) wing',
+    cpacs_path=CEASIOM_XPATH + '/geometry/analysis/wingSpan',
+)

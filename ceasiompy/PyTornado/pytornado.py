e41d24fe759493d120993d663ede8d931ba1885d
Aaron Dettmann <dettmann@kth.se>
2019-08-12 15:35:52 +0200

    Initialise module for PyTornado

---
+++ ceasiompy/PyTornado/pytornado.py
@@ -0,0 +1,51 @@
+"""
+CEASIOMpy: Conceptual Aircraft Design Software
+
+Developed by CFS ENGINEERING, 1015 Lausanne, Switzerland
+
+Small description of the script
+
+Python version: >=3.6
+
+| Author: Aaron Dettmann
+| Creation: 2019-08-12
+| Last modifiction: 2019-08-12
+"""
+
+import os
+
+from ceasiompy.utils.ceasiomlogger import get_logger
+from ceasiompy.utils.moduleinterfaces import check_cpacs_input_requirements
+from ceasiompy.ModuleTemplate.__specs__ import cpacs_inout
+
+log = get_logger(__file__.split('.')[0])
+
+# PyTornado is an external dependency. Assuming PyTornado is not installed.
+PYTORNADO_FOUND = True
+try:
+    import pytornado.stdfun as pyt
+except ModuleNotFoundError:
+    PYTORNADO_FOUND = False
+    log.warning("PyTornado was not found")
+
+
+if __name__ == '__main__':
+    log.info("Running PyTornado...")
+
+    # Abort if PyTornado is not properly installed
+    if not PYTORNADO_FOUND:
+        err_msg = "PyTornado was not found. Cannot run an analysis."
+        log.error(err_msg)
+        raise ModuleNotFoundError(err_msg)
+
+    # CEASIOMpy paths
+    MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
+    cpacs_path = MODULE_DIR + '/ToolInput/ToolInput.xml'
+    cpacs_out_path = MODULE_DIR + '/ToolOutput/ToolOutput.xml'
+
+    check_cpacs_input_requirements(cpacs_path, cpacs_inout, __file__)
+
+    # ========== PyTornado main analysis ==========
+    pass
+
+    log.info("PyTornado analysis completed")
--- doc/TODO.md
+++ doc/TODO.md
@@ -1,7 +1,6 @@
 # TODO
 
 ## CEASIOMpy project
-
 * Add a recognizable logo
 * Make installation easier (Tixi, Tigl, TIGLCreator, sumo, SU2, pyTornado, RCE, conda?)
     * Connecting CEASIOMpy with RCE?
@@ -9,15 +8,14 @@
     * Separate code from user working directory
 
 ## Module dependencies
-
 * Add `__specs__.py` files for each modules
+* `_specs.py` better than `__specs__`?
 * Add output for `ceasiompy.CLCalculator.__specs__`
 * Add General File in/out Class
 * Maybe create a function to check consistency (does same variable name are stored a the same xpath, ...)
 * Add kind of a GUI to enter missing input requirements...?
 
 ## Documentation
-
 * Host the documentation on ReadTheDocs (https://readthedocs.org/)?
 * Automatic syncronisation of CPACS input/output
 * Fix docstring translation/formatting
@@ -36,7 +34,6 @@
 * Folder structure for the results
 
 ## Modules
-
 * To adapt and integrate
     * Template Module
     * PyTornado
@@ -57,3 +54,8 @@
     * Upuload .json file
     * Add a script to automatically set the ToolDirectory path of each module
     * Small logo for each module
+
+## PyTornado module
+Update `specs` file
+    * Airfoil
+    * Aeroperformance map

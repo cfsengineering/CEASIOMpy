395ac39027b8b99e516fe3028a1db7176083079c
Aaron Dettmann <dettmann@kth.se>
2019-09-10 11:51:21 +0200

    Updated 'PyTornado' and 'ModuleTemplate' __specs__

--- ceasiompy/PyTornado/__specs__.py
+++ ceasiompy/PyTornado/__specs__.py
@@ -7,8 +7,89 @@ cpacs_inout = CPACSInOut()
 
 REFS_XPATH = AIRCRAFT_XPATH + '/model/reference'
 WING_XPATH = AIRCRAFT_XPATH + '/model/wings'
+XPATH_PYTORNADO = '/cpacs/toolspecific/pytornado'
 
 #===== Input =====
+
+cpacs_inout.add_input(
+    var_name='',
+    var_type=str,
+    default_value=None,
+    unit=None,
+    descr="Name of the aero map to evaluate",
+    cpacs_path=XPATH_PYTORNADO + '/aeroMapUID',
+    gui=True,
+    gui_name='Aero map name',
+    gui_group=None,
+)
+
+# ----- Discretisation -----
+# TO BE IMPROVED IN NEW PYTORNADO VERSION
+cpacs_inout.add_input(
+    var_name='',
+    var_type=int,
+    default_value=20,
+    unit=None,
+    descr="The number of chordwise VLM panels",
+    cpacs_path=XPATH_PYTORNADO + '/vlm_autopanels_c',
+    gui=True,
+    gui_name='Number of chordwise panels',
+    gui_group='Dicretisation',
+)
+
+cpacs_inout.add_input(
+    var_name='',
+    var_type=int,
+    default_value=5,
+    unit=None,
+    descr="The number of spanwise VLM panels",
+    cpacs_path=XPATH_PYTORNADO + '/vlm_autopanels_s',
+    gui=True,
+    gui_name='Number of spanwise panels',
+    gui_group='Dicretisation',
+)
+
+# ----- Plots -----
+for plot_name in ['lattice', 'geometry', 'results', 'matrix_downwash']:
+    cpacs_inout.add_input(
+        var_name='',
+        var_type=bool,
+        default_value=False,
+        unit=None,
+        descr=f"Show a {plot_name} plot (program will pause to show)",
+        cpacs_path=XPATH_PYTORNADO + f'/plot/{plot_name}/show',
+        gui=True,
+        gui_name='Show plot',
+        gui_group=f'{plot_name.capitalize()} plot',
+    )
+
+    cpacs_inout.add_input(
+        var_name='',
+        var_type=bool,
+        default_value=False,
+        unit=None,
+        descr=f"Show a {plot_name} plot",
+        cpacs_path=XPATH_PYTORNADO + f'/plot/{plot_name}/save',
+        gui=True,
+        gui_name='Save plot',
+        gui_group=f'{plot_name.capitalize()} plot',
+    )
+
+    # TODO: add optional settings
+
+# ----- Save other results -----
+for save_name in ['global', 'panelwise', 'aeroperformance']:
+    cpacs_inout.add_input(
+        var_name='',
+        var_type=bool,
+        default_value=False,
+        unit=None,
+        descr=f"Save PyTornado '{save_name}' results",
+        cpacs_path=XPATH_PYTORNADO + f'/save_results/{save_name}',
+        gui=True,
+        gui_name=f'Save {save_name.capitalize()}',
+        gui_group=f'Save CPACS external results',
+    )
 
 cpacs_inout.add_input(
     var_name='x_CG',
@@ -74,14 +155,4 @@ RCE = {
     "exec": "pwd\npython runpytornado.py",
     "author": "Aaron Dettmann",
     "email": "dettmann@kth.se",
-}
-
-XPATH_PYTORNADO = '/cpacs/toolspecific/pytornado'
-
-GUI_SETTINGS = {
-    'Aero map name': ['', str, None, XPATH_PYTORNADO + '/aeroMapUID', 'Name of the aero map to evaluate'],
-    'Chordwise panels': [10, int, '1', XPATH_PYTORNADO + '/vlm_autopanels_c', 'Number of chordwise panels'],
-    'Spanwise panels': [5, int, '1', XPATH_PYTORNADO + '/vlm_autopanels_s', 'Number of spanwise panels'],
-    'Show plot lattice': [False, bool, None, XPATH_PYTORNADO + '/plot/lattice/show', 'Show lattice plot?'],
-    'Save plot lattice': [False, bool, None, XPATH_PYTORNADO + '/plot/lattice/save', 'Save lattice plot?'],
 }

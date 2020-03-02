77907b19f81b6a0ed4418bcd31d84245d1231e42
CFS Engineering <cfse@cfs9.cfs-internal.ch>
2020-03-02 11:01:42 +0100

    Corrected workflowfunction test file

--- test/utils/TestWorkflowFunctions/test_workflowfunctions.py
+++ test/utils/TestWorkflowFunctions/test_workflowfunctions.py
@@ -1,16 +1,74 @@
- 
 """
-Created on Tue Feb 25 15:59:13 2020
+CEASIOMpy: Conceptual Aircraft Design Software
 
-@author: Aidan Jungo
+Developed by CFS ENGINEERING, 1015 Lausanne, Switzerland
 
-Module containing the utilitary functions for the workflowcreator and optimization modules
+Module to test the workflowfunctions module
 
-TODO:
-    - Investigate why the stp models are not or badly exported
-    - Add the function to make a caption
+Python version: >=3.6
+
+| Author: Vivien Riolo
+| Creation: 26-02-2020
+| Last modification: 26-02-2020
+
 """
 
 #==============================================================================
 #   IMPORTS
 #==============================================================================
+
+import os
+
+from ceasiompy.utils.workflowfunctions import copy_module_to_module
+from ceasiompy.utils.ceasiomlogger import get_logger
+log = get_logger(__file__.split('.')[0])
+
+import ceasiompy.__init__
+LIB_DIR = os.path.dirname(ceasiompy.__init__.__file__)
+
+# Default CPACS file to test
+MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
+CPACS_IN_PATH = os.path.join(MODULE_DIR,'ToolInput','ToolInput.xml')
+CPACS_OUT_PATH = os.path.join(MODULE_DIR,'ToolOutput','ToolOutput.xml')
+
+#==============================================================================
+#   FUNCTIONS
+#==============================================================================
+
+def test_copy_module_to_module():
+    """
+    Tests the copy_module_to_module function
+    """
+    
+    module_from = MODULE_DIR
+    module_to = MODULE_DIR
+    io_from = "in"
+    io_to = "out"
+    copy_module_to_module(module_from, io_from, module_to, io_to)
+
+    # Read Input and Ouput CPACS file as text, to compare them
+    with open(CPACS_IN_PATH) as file_in:
+        lines_cpacs_in = file_in.readlines()
+    with open(CPACS_OUT_PATH) as file_out:
+        lines_cpacs_out = file_out.readlines()
+
+    assert lines_cpacs_in == lines_cpacs_out
+
+# def test_run_subworkflow():
+#     """
+#     Tests the run_subworkflow function
+#     """
+    
+#     module_list = []
+
+#     run_subworkflow(module_list)
+     
+#==============================================================================
+#    MAIN
+#==============================================================================
+
+if __name__ == '__main__':
+
+    log.info('Test WorkflowFunctions')
+    log.info('To run test use the following command:')
+    log.info('>> pytest -v')

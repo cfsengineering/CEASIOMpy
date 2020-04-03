f8a8a94bae4c71f09a95aef343287a397f0b956e
aidanjungo <aidan@cfse.ch>
2020-04-03 14:49:58 +0200

    Small improvments

--- ceasiompy/WorkflowCreator/workflowcreator.py
+++ ceasiompy/WorkflowCreator/workflowcreator.py
@@ -33,6 +33,8 @@ import ceasiompy.utils.ceasiompyfunctions as ceaf
 import ceasiompy.utils.cpacsfunctions as cpsf
 import ceasiompy.utils.moduleinterfaces as mi
 
+from ceasiompy.SettingsGUI.settingsgui import create_gui
+
 from ceasiompy.Optimisation.optimisation import optimize
 
 from ceasiompy.utils.ceasiomlogger import get_logger
@@ -63,28 +65,32 @@ if __name__ == '__main__':
     wkdir = ceaf.get_wkdir_or_create_new(tixi)
     cpsf.close_tixi(tixi,cpacs_path)
 
-
     ####### USER INPUT ########
     # Available Module:
 
     # Settings: 'SettingsGUI'
     # Geometry and mesh: 'CPACSCreator','CPACS2SUMO','SUMOAutoMesh'
     # Weight and balance: 'WeightConventional','WeightUnconventional','BalanceConventional','BalanceUnconventional'
-    # Aerodynamics: 'CLCalculator','PyTornado','SkinFriction','PlotAeroCoefficients','SU2Run'
+    # Aerodynamics: 'CLCalculator','PyTornado','SkinFriction','PlotAeroCoefficients','SU2MeshDef','SU2Run'
     # Mission analysis: 'Range'
 
     # Copy-Paste the module you want to execute, they will be run in order
-    module_pre = ['SettingsGUI','WeightConventional','SkinFriction','CLCalculator','Range']
-
-    module_optim = ['WeightConventional','PyTornado']
-    module_post = []
-    optim =  True
+    settings = True
+    module_pre = ['WeightConventional','PyTornado','PlotAeroCoefficients']
+    optim =  False
     doe = False
+    module_optim = ['WeightConventional','SU2Run']
+    module_post = []
 
-    # Pre Workflow (only run once)------------------------
-    wkf.run_subworkflow(module_pre,cpacs_path)
+    # Run Pre-Workflow (with or without SettingsGUI)------------------------
+    if settings:
+        settings_out_path = mi.get_tooloutput_file_path('SettingsGUI')
+        create_gui(cpacs_path,settings_out_path,module_pre)
+        wkf.run_subworkflow(module_pre,settings_out_path)
+    else:
+        wkf.run_subworkflow(module_pre,cpacs_path)
 
-    # Optim Workflow ------------------------------------
+    # Run Optimisation Workflow ------------------------------------
     if optim:
         wkf.copy_module_to_module(module_pre[-1],'out','CPACSUpdater','in')
         #run_optimizer()
@@ -99,9 +105,13 @@ if __name__ == '__main__':
             shutil.copy(mi.get_tooloutput_file_path(module_optim[-1]),cpacs_path_out)
 
     elif doe:
+        # TODO: Design Of Experiment
         pass
 
     else:
+
         shutil.copy(mi.get_tooloutput_file_path(module_pre[-1]),cpacs_path_out)
+
+    # if module
 
     log.info('----- End of ' + os.path.basename(__file__) + ' -----')

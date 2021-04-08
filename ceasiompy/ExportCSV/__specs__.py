f732d82c9ed2a2ed0d3af99eaf503df79258a984
aidanjungo <aidan@cfse.ch>
2021-04-08 13:28:08 +0200

    New module to export aeromaps automatically

---
+++ ceasiompy/ExportCSV/__specs__.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from ceasiompy.utils.moduleinterfaces import CPACSInOut, AIRCRAFT_XPATH
+
+EXPORT_XPATH = '/cpacs/toolspecific/CEASIOMpy/export'
+
+# ===== RCE integration =====
+
+RCE = {
+    "name": "ExportCSV",
+    "description": "Module to export Aeromap to CSV",
+    "exec": "pwd\npython exportcsv.py",
+    "author": "Aidan Jungo",
+    "email": "aidan.jungo@cfse.ch",
+}
+
+# ===== CPACS inputs and outputs =====
+
+cpacs_inout = CPACSInOut()
+
+include_gui = False
+
+# ----- Input -----
+
+cpacs_inout.add_input(
+    var_name='',
+    var_type=list,
+    default_value=None,
+    descr='List of aeroMap to plot',
+    xpath=EXPORT_XPATH + '/aeroMapToExport',
+    gui=True,
+    gui_name='__AEROMAP_CHECHBOX',
+    # gui_group='Multipe aeromap'
+)
+
+# ----- Output -----

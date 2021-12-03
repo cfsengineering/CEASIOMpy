0199c2d7301c87575b2096f8966686763b4095c0
aidanjungo <aidan@cfse.ch>
2021-12-03 16:14:14 +0100

    fix: lint errors in CLCalculator

--- ceasiompy/CLCalculator/__specs__.py
+++ ceasiompy/CLCalculator/__specs__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from ceasiompy.utils.moduleinterfaces import CPACSInOut
-from ceasiompy.utils.xpath import (REF_XPATH, CLCALC_XPATH, SU2_XPATH)
+from ceasiompy.utils.xpath import REF_XPATH, CLCALC_XPATH, SU2_XPATH
 
 # ===== RCE integration =====
 
@@ -21,104 +21,104 @@ cpacs_inout = CPACSInOut()
 # ===== Input =====
 
 cpacs_inout.add_input(
-    var_name='mass_type',
+    var_name="mass_type",
     var_type=list,
-    default_value= ['mTOM', 'mZFM', 'Custom','% fuel mass'],
+    default_value=["mTOM", "mZFM", "Custom", "% fuel mass"],
     unit=None,
-    descr='Type of mass to use for CL calculation',
-    xpath=CLCALC_XPATH +'/massType',
+    descr="Type of mass to use for CL calculation",
+    xpath=CLCALC_XPATH + "/massType",
     gui=True,
-    gui_name='Type',
-    gui_group='Mass'
+    gui_name="Type",
+    gui_group="Mass",
 )
 
 cpacs_inout.add_input(
-    var_name='custom_mass',
+    var_name="custom_mass",
     var_type=float,
     default_value=0.0,
-    unit='kg',
-    descr='Mass value if Custom is selected',
-    xpath=CLCALC_XPATH +'/customMass',
+    unit="kg",
+    descr="Mass value if Custom is selected",
+    xpath=CLCALC_XPATH + "/customMass",
     gui=True,
-    gui_name='Custom mass',
-    gui_group='Mass',
+    gui_name="Custom mass",
+    gui_group="Mass",
 )
 
 cpacs_inout.add_input(
-    var_name='percent_fuel_mass',
+    var_name="percent_fuel_mass",
     var_type=float,
     default_value=100,
-    unit='-',
-    descr='Percentage of fuel mass between mTOM and mZFM, if % fuel mass is selected',
-    xpath=CLCALC_XPATH + '/percentFuelMass',
+    unit="-",
+    descr="Percentage of fuel mass between mTOM and mZFM, if % fuel mass is selected",
+    xpath=CLCALC_XPATH + "/percentFuelMass",
     gui=True,
-    gui_name='Percent fuel mass',
-    gui_group='Mass',
+    gui_name="Percent fuel mass",
+    gui_group="Mass",
 )
 
 cpacs_inout.add_input(
-    var_name='cruise_mach',
+    var_name="cruise_mach",
     var_type=float,
     default_value=0.78,
-    unit='1',
-    descr='Aircraft cruise Mach number',
-    xpath=CLCALC_XPATH + '/cruiseMach',
+    unit="1",
+    descr="Aircraft cruise Mach number",
+    xpath=CLCALC_XPATH + "/cruiseMach",
     gui=True,
-    gui_name='Mach',
-    gui_group='Cruise',
+    gui_name="Mach",
+    gui_group="Cruise",
 )
 
 cpacs_inout.add_input(
-    var_name='cruise_alt',
+    var_name="cruise_alt",
     var_type=float,
     default_value=12000.0,
-    unit='m',
-    descr='Aircraft cruise altitude',
-    xpath=CLCALC_XPATH + '/cruiseAltitude',
+    unit="m",
+    descr="Aircraft cruise altitude",
+    xpath=CLCALC_XPATH + "/cruiseAltitude",
     gui=True,
-    gui_name='Altitude',
-    gui_group='Cruise',
+    gui_name="Altitude",
+    gui_group="Cruise",
 )
 
 cpacs_inout.add_input(
-    var_name='load_fact',
+    var_name="load_fact",
     var_type=float,
     default_value=1.05,
-    unit='1',
-    descr='Aircraft cruise altitude',
-    xpath=CLCALC_XPATH + '/loadFactor',
+    unit="1",
+    descr="Aircraft cruise altitude",
+    xpath=CLCALC_XPATH + "/loadFactor",
     gui=True,
-    gui_name='Load Factor',
-    gui_group='Cruise',
+    gui_name="Load Factor",
+    gui_group="Cruise",
 )
 
 cpacs_inout.add_input(
-    var_name='ref_area',
+    var_name="ref_area",
     var_type=float,
     default_value=None,
-    unit='m^2',
-    descr='Aircraft reference area',
-    xpath=REF_XPATH + '/area',
+    unit="m^2",
+    descr="Aircraft reference area",
+    xpath=REF_XPATH + "/area",
     gui=False,
     gui_name=None,
     gui_group=None,
 )
 
 
-#===== Output =====
+# ===== Output =====
 
 cpacs_inout.add_output(
-    var_name='target_cl',
+    var_name="target_cl",
     default_value=None,
-    unit='1',
-    descr='Value of CL to achieve to have a level flight with the given conditions',
-    xpath=SU2_XPATH + '/targetCL',
+    unit="1",
+    descr="Value of CL to achieve to have a level flight with the given conditions",
+    xpath=SU2_XPATH + "/targetCL",
 )
 
 cpacs_inout.add_output(
-    var_name='fixed_cl',
+    var_name="fixed_cl",
     default_value=None,
-    unit='-',
-    descr='FIXED_CL_MODE parameter for SU2',
-    xpath=SU2_XPATH + '/fixedCL',
+    unit="-",
+    descr="FIXED_CL_MODE parameter for SU2",
+    xpath=SU2_XPATH + "/fixedCL",
 )

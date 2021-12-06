8de68aa3cd6abf9ac60fd4c7dca9a4d7012dd9df
aidanjungo <aidan@cfse.ch>
2021-12-06 09:26:26 +0100

    fix: lint errors in PlotAeroCoef

--- ceasiompy/PlotAeroCoefficients/__specs__.py
+++ ceasiompy/PlotAeroCoefficients/__specs__.py
@@ -21,59 +21,59 @@ RCE = {
 cpacs_inout = CPACSInOut()
 
 
-#===== Input =====
+# ===== Input =====
 
 cpacs_inout.add_input(
-    var_name='',
+    var_name="",
     var_type=list,
     default_value=None,
-    descr='List of aeroMap to plot',
-    xpath=PLOT_XPATH + '/aeroMapToPlot',
+    descr="List of aeroMap to plot",
+    xpath=PLOT_XPATH + "/aeroMapToPlot",
     gui=True,
-    gui_name='__AEROMAP_CHECHBOX',
+    gui_name="__AEROMAP_CHECHBOX",
     # gui_group='Multipe aeromap'
 )
 
 cpacs_inout.add_input(
-    var_name='alt_crit',
+    var_name="alt_crit",
     var_type=str,
-    default_value='None',
-    descr='Altitude inclusion criteria',
-    xpath=PLOT_XPATH + '/criterion/alt',
+    default_value="None",
+    descr="Altitude inclusion criteria",
+    xpath=PLOT_XPATH + "/criterion/alt",
     gui=True,
-    gui_name='Altitdue criteria',
-    gui_group='Plot vs AoA'
+    gui_name="Altitdue criteria",
+    gui_group="Plot vs AoA",
 )
 
 cpacs_inout.add_input(
-    var_name='mach_crit',
+    var_name="mach_crit",
     var_type=str,
-    default_value='None',
-    descr='Mach inclusion criteria',
-    xpath=PLOT_XPATH + '/criterion/mach',
+    default_value="None",
+    descr="Mach inclusion criteria",
+    xpath=PLOT_XPATH + "/criterion/mach",
     gui=True,
-    gui_name='Mach criteria',
-    gui_group='Plot vs AoA'
+    gui_name="Mach criteria",
+    gui_group="Plot vs AoA",
 )
 
 cpacs_inout.add_input(
-    var_name='aos_crit',
+    var_name="aos_crit",
     var_type=str,
-    default_value='None',
-    descr='Angle of Sideslip (AoS) inclusion criteria',
-    xpath=PLOT_XPATH + '/criterion/aos',
+    default_value="None",
+    descr="Angle of Sideslip (AoS) inclusion criteria",
+    xpath=PLOT_XPATH + "/criterion/aos",
     gui=True,
-    gui_name='AoS criteria',
-    gui_group='Plot vs AoA'
+    gui_name="AoS criteria",
+    gui_group="Plot vs AoA",
 )
 
 cpacs_inout.add_input(
-    var_name='manual_selct',
+    var_name="manual_selct",
     var_type=bool,
     default_value=False,
-    descr='Angle of Sideslip (AoS) inclusion criteria',
-    xpath=PLOT_XPATH + '/manualSelection',
+    descr="Angle of Sideslip (AoS) inclusion criteria",
+    xpath=PLOT_XPATH + "/manualSelection",
     gui=True,
-    gui_name='Select AeroMaps manualy',
-    gui_group='Other options'
+    gui_name="Select AeroMaps manualy",
+    gui_group="Other options",
 )

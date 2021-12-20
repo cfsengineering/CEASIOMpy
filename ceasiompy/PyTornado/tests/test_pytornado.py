15b858e99380586122deb3ee6eb24ea07b729b5b
aidanjungo <aidan@cfse.ch>
2021-12-20 15:46:08 +0100

    Move PyTornado tests and apply black

--- ceasiompy/PyTornado/tests/test_pytornado.py
+++ ceasiompy/PyTornado/tests/test_pytornado.py
@@ -3,14 +3,14 @@ import shutil
 from contextlib import contextmanager
 import importlib
 
-PYTORNADO_MAIN_MODULE = 'ceasiompy.PyTornado.runpytornado'
+PYTORNADO_MAIN_MODULE = "ceasiompy.PyTornado.runpytornado"
 HERE = os.path.dirname(os.path.abspath(__file__))
-CPACS_TEST_FILE=os.path.join(HERE, 'cpacs_test_file.xml')
+CPACS_TEST_FILE = os.path.join(HERE, "cpacs_test_file.xml")
 
 
 @contextmanager
 def run_module_test_locally(module_name, test_dir):
-    """ Context manager which copies a single module
+    """Context manager which copies a single module
 
     Args:
         module_name (str): Full 'path' of module which is to be loaded
@@ -30,7 +30,7 @@ def run_module_test_locally(module_name, test_dir):
 def test_basic_run():
     """Make sure that the PyTornado run successfully"""
 
-    shutil.copy(src=CPACS_TEST_FILE, dst=os.path.join(HERE, 'ToolInput', 'ToolInput.xml'))
+    shutil.copy(src=CPACS_TEST_FILE, dst=os.path.join(HERE, "ToolInput", "ToolInput.xml"))
 
     with run_module_test_locally(PYTORNADO_MAIN_MODULE, HERE) as main:
-        os.system(f'python {main}')
+        os.system(f"python {main}")

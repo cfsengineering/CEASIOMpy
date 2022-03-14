83dc662be59605748632d1c88bf8a7cd3ad4f9db
aidanjungo <aidan@cfse.ch>
2022-03-14 15:20:41 +0100

    add install script for ceasiompy

---
+++ install_scripts/Ubuntu/install_ceasiompy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script to install CEASIOMpy on Ubuntu 20.04 and  Mint 20.3
+
+sudo apt install g++
+sudo apt install libtbb2
+
+cd ../..
+conda env create -f environment.yml
+conda activate ceasiompy
+pip install -e .

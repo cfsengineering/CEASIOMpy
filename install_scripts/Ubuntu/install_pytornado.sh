7640c5ec707cdb2ba45bbd20ad2583f836d38407
aidanjungo <aidan@cfse.ch>
2022-03-14 15:28:59 +0100

    add install script for Pytornado

---
+++ install_scripts/Ubuntu/install_pytornado.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script to install PyTornado on Ubuntu 20.04 and  Mint 20.3
+
+# Activate conda environment to install PyTornado in it
+conda activate ceasiompy
+
+cd ../../..
+
+# Download PyTornado from GitHub and install it
+git clone https://github.com/airinnova/pytornado.git
+cd pytornado
+pip install --user .

d4e7b8a68e6f4a9900681fc8bd1a03afd42fb033
aidanjungo <aidan@cfse.ch>
2022-03-15 15:44:43 +0100

    Add install script for Paraview on Ubuntu

---
+++ install_scripts/Ubuntu/install_paraview.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Script to install Paraview on Ubuntu 20.04 and  Mint 20.3
+
+# Get install dir from input if it exists
+if [ $# -gt 0 ]; then
+    install_dir="$1/CEASIOMpy_install_dir"
+else
+    install_dir="$(pwd)/CEASIOMpy_install_dir"
+fi
+
+echo "Creating install directory..."
+mkdir -p "$install_dir"
+cd $install_dir
+
+echo "Downloading Paraview..."
+wget https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v5.10&type=binary&os=Linux&downloadFile=ParaView-5.10.0-MPI-Linux-Python3.9-x86_64.tar.gz
+tar zxvf ParaView-5.10.0-MPI-Linux-Python3.9-x86_64
+
+echo "Adding sybolic for Paraview"
+sudo ln -s $install_dir/ParaView-5.10.0-MPI-Linux-Python3.9-x86_64/bin/paraview  /usr/bin/paraview

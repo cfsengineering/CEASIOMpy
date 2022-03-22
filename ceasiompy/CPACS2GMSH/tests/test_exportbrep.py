20c19e22e5c8703e6a434ae4d10eaeeef8a5a2c9
Tony Govoni <tony.govoni@epfl.ch>
2022-03-22 12:14:35 +0100

    add test for exportbrep.py

--- ceasiompy/CPACS2GMSH/tests/test_exportbrep.py
+++ ceasiompy/CPACS2GMSH/tests/test_exportbrep.py
@@ -18,15 +18,22 @@ Python version: >=3.7
 
 import os
 import sys
+from pathlib import Path
 
 import pytest
+import tigl3.configuration
+from ceasiompy.utils.ceasiomlogger import get_logger
 from pytest import approx
+from tigl3 import tigl3wrapper
+from tigl3.import_export_helper import export_shapes
+from tixi3 import tixi3wrapper
 
+from ceasiompy.CPACS2GMSH.cpacs2gmsh import exportbrep
 
 # Default CPACS file to test
 MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
 CPACS_IN_PATH = os.path.join(MODULE_DIR, "ToolInput", "simpletest_cpacs.xml")
-CPACS_OUT_PATH = os.path.join(MODULE_DIR, "ToolOutput", "ToolOutput.xml")
+TEST_OUT_PATH = os.path.join(MODULE_DIR, "ToolOutput")
 
 
 # ==============================================================================
@@ -39,10 +46,18 @@ CPACS_OUT_PATH = os.path.join(MODULE_DIR, "ToolOutput", "ToolOutput.xml")
 # ==============================================================================
 
 
-def test_MyClass():
-    """Test Class 'MyClass'"""
-
-    assert 1 == 1
+def test_exportbrep():
+    """Test Class 'test_exportbrep'"""
+    exportbrep(CPACS_IN_PATH, TEST_OUT_PATH)
+    file_count = 0
+    for file in os.listdir(TEST_OUT_PATH):
+        if ".brep" in file:
+            file_count += 1
+    # erease generated file
+    for file in os.listdir(TEST_OUT_PATH):
+        if ".brep" in file:
+            os.remove(os.path.join(TEST_OUT_PATH, file))
+    assert file_count == 3  # simpletest_cpacs.xml containt only 3 airplane parts
 
 
 # ==============================================================================
@@ -51,6 +66,6 @@ def test_MyClass():
 
 if __name__ == "__main__":
 
-    print("Test ModuleTemplate")
+    print("Test CPACS2GMSH")
     print("To run test use the following command:")
     print(">> pytest -v")

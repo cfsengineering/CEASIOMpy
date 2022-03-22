cac2d691e3088d6ab6ea4840776262a3daee4f17
aidanjungo <aidan@cfse.ch>
2022-03-22 14:04:30 +0100

    fix: paraview download file name

--- install_scripts/Ubuntu/install_paraview.sh
+++ install_scripts/Ubuntu/install_paraview.sh
@@ -16,8 +16,8 @@ mkdir -p "$install_dir"
 cd $install_dir
 
 echo "Downloading Paraview..."
-wget -O ParaView-5.10.0-MPI-Linux-Python3.9-x86_64 "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v5.10&type=binary&os=Linux&downloadFile=ParaView-5.10.0-MPI-Linux-Python3.9-x86_64.tar.gz"
-tar zxvf ParaView-5.10.0-MPI-Linux-Python3.9-x86_64
+wget -O ParaView-5.10.0-MPI-Linux-Python3.9-x86_64.tar.gz "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v5.10&type=binary&os=Linux&downloadFile=ParaView-5.10.0-MPI-Linux-Python3.9-x86_64.tar.gz"
+tar zxvf ParaView-5.10.0-MPI-Linux-Python3.9-x86_64.tar.gz
 
 echo "Adding sybolic link for Paraview"
 sudo ln -s $(pwd)/ParaView-5.10.0-MPI-Linux-Python3.9-x86_64/bin/paraview  /usr/bin/paraview

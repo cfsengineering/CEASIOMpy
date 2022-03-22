a824fa7fe4ca2fa271829169a73603d87c4110a7
Aidan <53896963+aidanjungo@users.noreply.github.com>
2022-03-22 15:06:28 +0100

    Installation scripts for Ubuntu (#152)
    * Add install script for SUMO on Ubuntu
    
    * Add install script for Miniconda (wip)
    
    * Add install script for ceasiompy
    
    * Add install script for Pytornado
    
    * Add install script fro SU2 and mpich
    
    * Add install script for Paraview
    
    * Complete installation INSATALLATION.md for Ubuntu
    
    * Add basic step for Windows and MacOS in INSATALLATION.md

---
+++ installation/Ubuntu/install_pytornado.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Script to install PyTornado on Ubuntu 20.04 and  Mint 20.3
+
+current_dir="$(pwd)"
+
+# Activate conda environment to install PyTornado in it
+conda activate ceasiompy
+
+# Install PyTornado next to CEASIOMpy
+cd ../../..
+
+# Download PyTornado from GitHub and install it
+git clone https://github.com/airinnova/pytornado.git
+cd pytornado
+pip install -e .
+
+cd $current_dir

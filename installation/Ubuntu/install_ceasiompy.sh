31b52a9125318873f9ccaf6e4d336f046f6d8ef9
Aidan <53896963+aidanjungo@users.noreply.github.com>
2022-03-22 15:06:28 +0100

    Installation scripts for Ubuntu (#152)
    * Add install script for SUMO on Ubuntu
    
    * Add install script for Miniconda (wip)
    
    * Add install script for ceasiompy
    
    * Add install script for Pytornado
    
    * Add install script fro SU2 and mpich
    
    * Add install script for Paraview
    
    * Complete installation INSATALLATION.md for Ubuntu
    
    * Add basic step for Windows and MacOS in INSATALLATION.md

---
+++ installation/Ubuntu/install_ceasiompy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script to install CEASIOMpy on Ubuntu 20.04 and  Mint 20.3
+
+current_dir="$(pwd)"
+
+sudo apt install g++
+sudo apt install libtbb2
+
+cd ../..
+
+conda env create -f environment.yml
+conda activate ceasiompy
+pip install -e .
+
+cd $current_dir

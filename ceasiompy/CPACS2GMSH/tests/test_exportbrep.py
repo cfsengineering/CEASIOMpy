1555e2e4ca905df05983ba2839a3bf4c659991c8
Tony Govoni <tony.govoni@epfl.ch>
2022-04-25 16:20:44 +0200

    minor codestyle errors again

--- ceasiompy/CPACS2GMSH/tests/test_exportbrep.py
+++ ceasiompy/CPACS2GMSH/tests/test_exportbrep.py
@@ -17,7 +17,6 @@ Python version: >=3.7
 # ==============================================================================
 
 import os
-import pytest
 from cpacspy.cpacspy import CPACS
 
 from ceasiompy.CPACS2GMSH.func.exportbrep import export_brep

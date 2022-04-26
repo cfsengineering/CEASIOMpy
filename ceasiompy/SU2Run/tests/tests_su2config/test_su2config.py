e61efe50d8cfb1f9fc9cd28459301e03bccd2785
aidanjungo <aidan@cfse.ch>
2022-04-26 09:57:04 +0200

    fix mock get_install_path

--- ceasiompy/SU2Run/tests/tests_su2config/test_su2config.py
+++ ceasiompy/SU2Run/tests/tests_su2config/test_su2config.py
@@ -22,6 +22,9 @@ from pathlib import Path
 from unittest.mock import patch, mock_open
 
 from ceasiompy.SU2Run.func.su2config import get_su2_version, get_su2_config_template
+
+from ceasiompy.utils.ceasiompyutils import get_install_path
+
 from ceasiompy.utils.moduleinterfaces import get_module_path
 
 MODULE_DIR = Path(__file__).parent
@@ -50,8 +53,13 @@ def test_get_su2_version():
 
     mock_data = mock_open(read_data=mock_text)
 
+    # /home/jungo/github/CEASIOMpy/ceasiompy/SU2Run/func/su2config.py
+
     # TODO: When Python 3.10 will be used, with could use one "with" with parentheses
-    with patch("ceasiompy.utils.ceasiompyutils.get_install_path", return_value=None):
+    with patch(
+        "ceasiompy.SU2Run.func.su2config.get_install_path",
+        return_value={"SU2_CFD": "/mockpath/bin/SU2_CFD"},
+    ):
         with patch.object(Path, "exists", return_value=True):
             with patch("builtins.open", mock_data):
                 assert get_su2_version() == "9.9.9"
@@ -64,7 +72,10 @@ def test_get_su2_version():
     mock_data_no_version = mock_open(read_data=mock_text_no_version)
 
     # TODO: When Python 3.10 will be used, with could use one "with" with parentheses
-    with patch("ceasiompy.utils.ceasiompyutils.get_install_path", return_value=None):
+    with patch(
+        "ceasiompy.SU2Run.func.su2config.get_install_path",
+        return_value={"SU2_CFD": "/mockpath/bin/SU2_CFD"},
+    ):
         with patch.object(Path, "exists", return_value=True):
             with patch("builtins.open", mock_data_no_version):
                 assert get_su2_version() is None

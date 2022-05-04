9d7df330dc13f4729931abc29912288f0b415c27
aidanjungo <aidan@cfse.ch>
2022-05-04 15:10:33 +0200

    Apply Black when running the CI

--- run_ci.sh
+++ run_ci.sh
@@ -5,9 +5,9 @@
 
 echo -e "\n######### Running CI #########"
 
-# echo -e "\n## Running Black ## \n"
-# black .
-# TODO uncomment when some files will be cleaned
+echo -e "\n## Running Black ## \n"
+black .
+TODO uncomment when some files will be cleaned
 
 echo -e "\n## Running Flake8 ## \n"
 flake8

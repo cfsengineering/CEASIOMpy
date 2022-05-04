0888d0acd33f2e16903a97e0fe0a7a3c15803e78
aidanjungo <aidan@cfse.ch>
2022-05-04 15:37:21 +0200

    fix python version

--- run_ci.sh
+++ run_ci.sh
@@ -7,7 +7,6 @@ echo -e "\n######### Running CI #########"
 
 echo -e "\n## Running Black ## \n"
 black .
-TODO uncomment when some files will be cleaned
 
 echo -e "\n## Running Flake8 ## \n"
 flake8

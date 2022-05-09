96c29114c73d7cb989f1e4432fcf9d3816e0db4a
Aidan <53896963+aidanjungo@users.noreply.github.com>
2022-05-09 15:27:35 +0200

    Add integration tests (#190)
    * Get a default aeromap if nothing is selected
    
    * Use generic XPATH
    
    * Move local CI in /tests
    
    * Add function to get a reasonable number of cpu for SU2
    
    * Run integration tests from python
    
    * Add integration tests (install PyTornado and SU2)
    
    * Separate flake8, unit and integration test workflow
    
    * Cache conda env
    
    * Set Tetgen path in dwfsumo.conf
    
    * Update badges
    
    * Add option when running CI locally
    
    * Add CI option to CONTRIBUTING.md

--- installation/Ubuntu/install_sumo.sh
+++ installation/Ubuntu/install_sumo.sh
@@ -28,15 +28,28 @@ if ! grep "ethz.ch/ubuntu/" /etc/apt/sources.list ; then
     sudo  -- bash -c 'echo "deb http://ubuntu.ethz.ch/ubuntu/ bionic universe" >> /etc/apt/sources.list'
     sudo  -- bash -c 'echo "deb http://ubuntu.ethz.ch/ubuntu/ bionic-updates universe" >> /etc/apt/sources.list'
 fi
-sudo apt-get update
-sudo apt-get install libgfortran3
+sudo apt-get update -y
+sudo apt-get install -y libgfortran3
 
 echo "--> libpng12"
 sudo add-apt-repository ppa:linuxuprising/libpng12
-sudo apt update
-sudo apt install libpng12-0
+sudo apt update -y
+sudo apt install -y libpng12-0
 
 echo "Creating a symlink for sumo..."
 sudo ln -s "$install_dir/sumo-2.7.9/bin/dwfsumo" /usr/bin/sumo
+
+echo "Set Tetgen path in dwfsumo.conf..."
+if [ ! -e "~/.config/larosterna/dwfsumo.conf" ]; then
+    mkdir -p ~/.config/larosterna
+    echo "[General]" >> ~/.config/larosterna/dwfsumo.conf
+    echo "tetgenpath=$install_dir/sumo-2.7.9/bin/tetgen" >> ~/.config/larosterna/dwfsumo.conf
+else
+    if ! grep "tetgenpath" ~/.config/larosterna/dwfsumo.conf ; then
+        echo "tetgenpath=$install_dir/sumo-2.7.9/bin/tetgen" >> ~/.config/larosterna/dwfsumo.conf
+    else
+        sed -i "s/tetgenpath.*/tetgenpath=$install_dir/sumo-2.7.9/bin/tetgen/" ~/.config/larosterna/dwfsumo.conf
+    fi
+fi
 
 cd "$current_dir"
---
+++ tests/run_ci.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+
+Help()
+{
+   echo "CEASIOMpy run Continuous Integration tests (Unit and Integration)"
+   echo
+   echo "Syntax: ./run_ci [-f|g|h|i|u]"
+   echo "options:"
+   echo "-f     Fast mode (skip tests marked as 'slow')"
+   echo "-g     Run GUI tests, requiring a user interaction (not active by default)."
+   echo "-h     Print this help message."
+   echo "-i     Skip integration tests."
+   echo "-u     Skip unit tests."
+   echo
+}
+
+
+gui=false
+integration=true
+fast=false
+unit=true
+for i in "$@" 
+do
+    if [ "$i" == "-g" ]; then
+        gui=true;
+    fi
+    if [ "$i" == "-f" ]; then
+        fast=true;
+    fi
+    if [ "$i" == "-i" ]; then
+        integration=false;
+    fi
+    if [ "$i" == "-u" ]; then
+        unit=false;
+    fi
+    if [ "$i" == "-h" ] || [ "$i" == "--help" ]; then
+        Help
+        exit
+    fi
+done
+
+
+echo -e "\n######### CEASIOMpy CI #########"
+
+echo -e "\n## Black ## \n"
+black ../
+
+
+echo -e "\n## Flake8 ## "
+flake8 ../
+if [ $? == 0 ]; then
+    echo -e "Flake8 passed without error\n"
+fi
+
+
+echo -e "\n## Unit tests ##"
+if [ $unit == true ]; then
+    echo -e "\nRunning..."
+    pytest -v ../ceasiompy --cov-report html:unittest_cov_html --cov-report term --cov=../ceasiompy --cov-config=../pyproject.toml 
+    #--disable-warnings
+else
+    echo -e "Skipping...\n"
+fi
+
+
+echo -e "\n## Integration tests ##"
+if [ $integration == true ]; then
+
+    echo -e "Running..."
+
+    if [ "$gui" = true ]; then
+        if [ "$fast" = true ]; then
+            pytest -v . --cov-report html:integration_cov_html --cov-report term --cov=../ceasiompy --cov-config=../pyproject.toml -m "not slow" --disable-warnings
+        else
+            pytest -v . --cov-report html:integration_cov_html --cov-report term --cov=../ceasiompy --cov-config=../pyproject.toml --disable-warnings
+        fi
+    else
+        if [ "$fast" = true ]; then
+            pytest -v . --cov-report html:integration_cov_html --cov-report term --cov=../ceasiompy --cov-config=../pyproject.toml -m "not gui and not slow" --disable-warnings
+        else
+            pytest -v . --cov-report html:integration_cov_html --cov-report term --cov=../ceasiompy --cov-config=../pyproject.toml -m "not gui" --disable-warnings
+        fi
+    fi
+else
+    echo -e "Skipping...\n"
+fi
+
+
+#

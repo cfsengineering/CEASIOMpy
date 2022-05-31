e8242e8a035e7b70de27cd08c7a8ebb6dc97f06c
aidanjungo <aidan@cfse.ch>
2022-05-31 13:43:44 +0200

    Add integration test with engine and proeller

--- tests/test_itegration_tests.py
+++ tests/test_itegration_tests.py
@@ -33,6 +33,7 @@ from src.bin.ceasiompy_exec import run_modules_list
 MODULE_DIR = Path(__file__).parent
 WORKFLOW_TEST_DIR = Path(MODULE_DIR, "workflow_tests")
 CPACS_IN_PATH = Path(MODULE_DIR, "Test_input.xml")
+CPACS_IN_2_PATH = Path(MODULE_DIR, "Test_input2.xml")
 
 # Remove previous workflow directory and create new one
 if WORKFLOW_TEST_DIR.exists():
@@ -110,9 +111,22 @@ def test_integration_4():
     assert workflow_ends()
 
 
+@pytest.mark.slow
+@pytest.mark.skipif(not shutil.which("gmsh"), reason="GMSH not installed")
+@pytest.mark.skipif(not shutil.which("SU2_CFD"), reason="SU2_CFD not installed")
+def test_integration_5():
+
+    modules_to_run = ["CPACS2GMSH", "SU2Run"]
+
+    with change_working_dir(WORKFLOW_TEST_DIR):
+        run_modules_list([str(CPACS_IN_2_PATH), *modules_to_run])
+
+    assert workflow_ends()
+
+
 @pytest.mark.gui
 @pytest.mark.skipif(not shutil.which("pytornado"), reason="PyTornado not installed")
-def test_integration_5():
+def test_integration_6():
     """Integration test for module with GUI, requiring a user interaction"""
 
     modules_to_run = ["CPACSCreator", "SettingsGUI", "PyTornado", "PlotAeroCoefficients"]

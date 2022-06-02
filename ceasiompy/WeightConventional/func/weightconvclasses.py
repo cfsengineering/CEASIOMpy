e88a6f9302467ab3464cee3ce64ce27295985cb6
aidanjungo <aidan@cfse.ch>
2022-06-02 17:16:45 +0200

    Continue the new cabin class

--- ceasiompy/WeightConventional/func/weightconvclasses.py
+++ ceasiompy/WeightConventional/func/weightconvclasses.py
@@ -1,4 +1,7 @@
 import math
+from pathlib import Path
+
+from cpacspy.cpacsfunctions import get_value_or_default
 from ceasiompy.WeightConventional.func.weightutils import (
     CABIN_CREW_MASS,
     PASSENGER_MASS,
@@ -6,9 +9,76 @@ from ceasiompy.WeightConventional.func.weightutils import (
     PILOT_MASS,
     TOILET_LENGTH,
 )
+from ceasiompy.utils.ceasiomlogger import get_logger
+from ceasiompy.utils.ceasiompyutils import get_results_directory
+from ceasiompy.utils.commonxpath import (
+    WB_AISLE_WIDTH_XPATH,
+    WB_SEAT_LENGTH_XPATH,
+    WB_SEAT_WIDTH_XPATH,
+)
+
+log = get_logger()
 
 
-class NewWeightOutputs:
+# @property
+# def fuselage_thickness_ratio(self):
+#     return get_value_or_default(self.cpacs.tixi, WB_FUSELAGE_THICK_XPATH, 6.63)
+
+# @property
+# def cabin_width(self):
+
+#     return self.fuselage_width * (1 - self.fuselage_thickness_ratio / 100)
+
+
+def check_aisle_nb(abreast_nb):
+    """It is a static method and not property to avoid circular dependency."""
+
+    if abreast_nb > 10:
+        log.warning(
+            "Aisle number is greater than 10. It is outside the range of value define for"
+            "the WeightConventional module, try to use the WeightUnconventional module."
+        )
+
+    if abreast_nb < 1:
+        log.warning(
+            "Aisle number is less than 1. It is outside the range of value define for"
+            "the WeightConventional module, try to use the WeightUnconventional module."
+        )
+
+    if abreast_nb < 1:
+        return 0
+    elif abreast_nb <= 6:
+        return 1
+    else:
+        return 2
+
+
+def check_toilet_length(passenger_nb):
+    """It is a static method and not property to avoid circular dependency."""
+
+    toilet_nb = math.ceil(passenger_nb / PASSENGER_PER_TOILET)
+
+    return math.ceil(toilet_nb / 2) * TOILET_LENGTH
+
+
+def stringed_seat_row(abreast_nb):
+    """Return a string which represent the seat row disposition."""
+
+    if abreast_nb == 1:
+        return "X ||" + "\n"
+    elif abreast_nb <= 6:
+        return "X " * (abreast_nb // 2) + "|| " + "X " * (abreast_nb - abreast_nb // 2) + "\n"
+    elif abreast_nb == 7:
+        return "X " * 2 + "|| " + "X " * 3 + "|| " + "X " * 2 + "\n"
+    elif abreast_nb == 8:
+        return "X " * 2 + "|| " + "X " * 4 + "|| " + "X " * 2 + "\n"
+    elif abreast_nb == 9:
+        return "X " * 3 + "|| " + "X " * 3 + "|| " + "X " * 3 + "\n"
+    elif abreast_nb == 10:
+        return "X " * 3 + "|| " + "X " * 4 + "|| " + "X " * 3 + "\n"
+
+
+class Cabin:
     """
     The class contains all the aircraft mass and weight value relative to the
     weight analysis.
@@ -25,9 +95,9 @@ class NewWeightOutputs:
     mass_cargo (float): Extra possible payload [kg]
     zero_fuel_mass (float): Zero fuel mass [kg]
     crew_mass (float): Crew members total mass [kg]
-    abreast_nb (int): Number of abreasts.
+    abreast_nb (int): Number of abreast.
     row_nb (int): Number of rows.
-    pass_nb (int): Number of passengers.
+    passenger_nb (int): Number of passengers.
     toilet_nb (int): Number of toilets.
     crew_nb (int): Number of total crew members.
     cabin_crew_nb (int): Number of cabin crew members.
@@ -36,7 +106,11 @@ class NewWeightOutputs:
 
     """
 
-    def __init__(self):
+    def __init__(self, cpacs, cabin_length, cabin_width):
+
+        self.cpacs = cpacs
+        self.cabin_length = cabin_length
+        self.cabin_width = cabin_width
 
         self.wing_loading = 0
 
@@ -50,24 +124,38 @@ class NewWeightOutputs:
         self.mass_cargo = 0
         self.zero_fuel_mass = 0
 
-        # Equipements (TODO: calculate from cabin length and width)
-        self.abreast_nb = 0
-        self.row_nb = 0
-
         # People
         self.pilot_nb = 2
 
     @property
-    def pass_nb(self):
+    def abreast_nb(self):
+
+        seat_width = get_value_or_default(self.cpacs.tixi, WB_SEAT_WIDTH_XPATH, 0.525)
+        aisle_width = get_value_or_default(self.cpacs.tixi, WB_AISLE_WIDTH_XPATH, 0.42)
+
+        for i in range(10, 0, -1):
+            if (seat_width * i + aisle_width * check_aisle_nb(i)) <= self.cabin_width:
+                return i
+
+        log.warning("The cabin width is too narrow to fit one abreast and one aisle")
+        return 0
+
+    @property
+    def row_nb(self):
+
+        seat_length = get_value_or_default(self.cpacs.tixi, WB_SEAT_LENGTH_XPATH, 0.74)
+
+        for i in range(100, 0, -1):
+            passenger_nb_tmp = self.abreast_nb * i
+            if (seat_length * i + check_toilet_length(passenger_nb_tmp)) <= self.cabin_length:
+                return i
+
+        log.warning("The cabin length is too short to fit one row!")
+        return 0
+
+    @property
+    def passenger_nb(self):
         return self.abreast_nb * self.row_nb
-
-    @property
-    def toilet_nb(self):
-        return math.ceil(self.pass_nb / PASSENGER_PER_TOILET)
-
-    @property
-    def total_toilet_length(self):
-        return math.ceil(self.toilet_nb / 2) * TOILET_LENGTH
 
     @property
     def cabin_crew_nb(self):
@@ -77,19 +165,20 @@ class NewWeightOutputs:
                 -title14-vol3-sec121-391.xml
         """
 
-        if self.pass_nb >= 101:
-            return int(self.pass_nb / 50) + 1
-        elif self.pass_nb >= 51:
+        if self.passenger_nb >= 101:
+            return int(self.passenger_nb / 50) + 1
+        elif self.passenger_nb >= 51:
             return 2
-        elif self.pass_nb >= 19 and self.mass_payload <= 3400.0:
+        elif self.passenger_nb >= 19 and self.mass_payload <= 3400.0:
             return 1
-        elif self.pass_nb >= 9 and self.mass_payload > 3400.0:
+        elif self.passenger_nb >= 9 and self.mass_payload > 3400.0:
             return 1
         else:
             return 0
 
     @property
     def crew_nb(self):
+        # TODO: write in cpacs (same for the other properties)
         return self.pilot_nb + self.cabin_crew_nb
 
     @property
@@ -98,8 +187,33 @@ class NewWeightOutputs:
 
     @property
     def passenger_mass(self):
-        return self.pass_nb * PASSENGER_MASS
+        return self.passenger_nb * PASSENGER_MASS
 
     @property
     def people_mass(self):
         return self.crew_mass + self.passenger_mass
+
+    @property
+    def toilet_nb(self):
+        return math.ceil(self.passenger_nb / PASSENGER_PER_TOILET)
+
+    def write_seat_config(self):
+        """Write the seat configuration in a file in the result directory."""
+
+        result_dir = get_results_directory("WeightConventional")
+        output_file = Path(result_dir, "Seats_disposition.out")
+
+        lines = open(output_file, "w")
+        lines.write("\n---------------------------------------")
+        lines.write(f"\nAbreast nb.: {self.abreast_nb}")
+        lines.write(f"\nRow nb.: {self.row_nb}")
+        lines.write(f"\nSeats_nb : {self.passenger_nb}")
+        lines.write("-----------------------------------------")
+        lines.write("\nPossible seat configuration")
+        lines.write("\nSeat = X and Aisle = || ")
+        lines.write("\n---------------------------------------\n")
+
+        for _ in range(self.row_nb):
+            lines.write(stringed_seat_row(self.abreast_nb))
+
+        lines.close()
--- ceasiompy/WeightConventional/tests/test_weightconvclasses.py
+++ ceasiompy/WeightConventional/tests/test_weightconvclasses.py
@@ -17,13 +17,21 @@ Python version: >=3.7
 # =================================================================================================
 
 from pathlib import Path
-
 import pytest
 
-from ceasiompy.WeightConventional.func.weightconvclasses import NewWeightOutputs
+from cpacspy.cpacspy import CPACS
+
+from ceasiompy.WeightConventional.func.weightconvclasses import (
+    Cabin,
+    check_aisle_nb,
+    check_toilet_length,
+    stringed_seat_row,
+)
+from ceasiompy.WeightConventional.func.weightutils import TOILET_LENGTH
+from ceasiompy.utils.commonpaths import CPACS_FILES_PATH
 
 MODULE_DIR = Path(__file__).parent
-
+TEST_CPACS_IN = Path(CPACS_FILES_PATH, "D150_simple.xml")
 
 # =================================================================================================
 #   CLASSES
@@ -32,20 +40,15 @@ MODULE_DIR = Path(__file__).parent
 
 class TestWeightConv:
 
-    weight_conv = NewWeightOutputs()
+    cpacs = CPACS(TEST_CPACS_IN)
+
+    weight_conv = Cabin(cpacs, cabin_length=22.5, cabin_width=4.0)
 
     def test_mass_properties(self):
 
-        assert self.weight_conv.cabin_crew_nb == 0
-        assert self.weight_conv.crew_nb == 2
-        assert self.weight_conv.crew_mass == 204.0
-        assert self.weight_conv.passenger_mass == 0.0
-        assert self.weight_conv.people_mass == 204.0
-        assert self.weight_conv.toilet_nb == 0
-        assert self.weight_conv.total_toilet_length == 0.0
-
-        self.weight_conv.abreast_nb = 6
-        self.weight_conv.row_nb = 25
+        assert self.weight_conv.abreast_nb == 6
+        assert self.weight_conv.row_nb == 25
+        assert self.weight_conv.passenger_nb == 150
 
         assert self.weight_conv.cabin_crew_nb == 4
         assert self.weight_conv.crew_nb == 6
@@ -53,12 +56,42 @@ class TestWeightConv:
         assert self.weight_conv.passenger_mass == 15_750.0
         assert self.weight_conv.people_mass == 16_226.0
         assert self.weight_conv.toilet_nb == 3
-        assert self.weight_conv.total_toilet_length == 3.8
+
+        self.weight_conv.write_seat_config()
 
 
 # =================================================================================================
 #   FUNCTIONS
 # =================================================================================================
+
+
+def test_check_aisle_nb():
+
+    assert check_aisle_nb(0) == 0
+    assert check_aisle_nb(1) == 1
+    assert check_aisle_nb(6) == 1
+    assert check_aisle_nb(7) == 2
+    assert check_aisle_nb(10) == 2
+
+
+def test_check_toilet_length():
+
+    assert check_toilet_length(0) == 0
+    assert check_toilet_length(1) == TOILET_LENGTH
+    assert check_toilet_length(100) == TOILET_LENGTH
+    assert check_toilet_length(101) == 2 * TOILET_LENGTH
+    assert check_toilet_length(201) == 3 * TOILET_LENGTH
+
+
+def test_stringed_seat_row():
+    """Test function 'stringed_seat_row'"""
+
+    assert stringed_seat_row(1) == "X ||\n"
+    assert stringed_seat_row(2) == "X || X \n"
+    assert stringed_seat_row(3) == "X || X X \n"
+    assert stringed_seat_row(4) == "X X || X X \n"
+    assert stringed_seat_row(5) == "X X || X X X \n"
+    assert stringed_seat_row(7) == "X X || X X X || X X \n"
 
 
 # =================================================================================================

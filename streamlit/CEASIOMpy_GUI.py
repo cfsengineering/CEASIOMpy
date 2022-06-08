d68665e1ae74c49c7b60984e917e1608c0f9b71a
aidanjungo <aidan@cfse.ch>
2022-06-08 17:08:03 +0200

    Workflow page (wip)

--- streamlit/CEASIOMpy_GUI.py
+++ streamlit/CEASIOMpy_GUI.py
@@ -1,11 +1,18 @@
 import streamlit as st
+from pathlib import Path
+from ceasiompy.utils.commonpaths import CEASIOMPY_PATH
 
+
+st.title("CEASIOMpy")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.title("CEASIOMpy")
-    uploaded_file = st.file_uploader("Select a CPACS file", type=["xml"])
+    st.markdown("Use the sidebar to navigate to the different pages.")
+
+    st.markdown("[Github repository](https://github.com/cfsengineering/CEASIOMpy)")
+
+    # Add button to change page when available
 
 with col2:
-    st.image("../documents/logos/CEASIOMpy_main_logos.png", width=400)
+    st.image(str(Path(CEASIOMPY_PATH, "documents/logos/CEASIOMpy_main_logos.png")), width=400)
--- streamlit/pages/03_Results.py
+++ streamlit/pages/03_Results.py
@@ -1,3 +1,16 @@
 import streamlit as st
+from pathlib import Path
 
+st.set_page_config(page_title="Results", page_icon="📈")
 st.title("Results")
+
+results_dir = Path(st.session_state.workflow.current_wkflow_dir, "Results")
+
+st.text(f"The Results dir is: {results_dir}")
+
+for dir in results_dir.iterdir():
+    if dir.is_dir():
+        with st.expander(dir.name, expanded=False):
+            st.text(f"")
+            for file in dir.iterdir():
+                st.text(file)

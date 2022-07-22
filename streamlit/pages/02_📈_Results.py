6b71b1a8c294dbea03fb6d874f7ab65b53b2db9f
aidanjungo <aidan@cfse.ch>
2022-07-22 16:08:14 +0200

    result page (wip)

--- streamlit/pages/02_📈_Results.py
+++ streamlit/pages/02_📈_Results.py
@@ -6,14 +6,21 @@ import streamlit as st
 st.set_page_config(page_title="Results", page_icon="📈")
 st.title("Results")
 
-# results_dir = Path(st.session_state.workflow.current_wkflow_dir, "Results")
-# st.text(f"The Results dir is: {results_dir}")
-# for dir in results_dir.iterdir():
-#     if dir.is_dir():
-#         with st.expander(dir.name, expanded=False):
-#             st.text(f"")
-#             for file in dir.iterdir():
-#                 st.text(file)
+# TODO: all this part should be improved
+results_dir = Path(st.session_state.workflow.working_dir)
+last_workflow = 0
+for dir in results_dir.iterdir():
+    if "Workflow_" in str(dir):
+        last_workflow = max(last_workflow, int(str(dir).split("_")[-1]))
+
+results_dir = Path(results_dir, f"Workflow_0{last_workflow}", "Results")
+st.text(f"The Results dir is: {str(results_dir)}")
+for dir in results_dir.iterdir():
+    if dir.is_dir():
+        with st.expander(dir.name, expanded=False):
+            st.text(f"")
+            for file in dir.iterdir():
+                st.markdown(file)
 
 CEASIOMPY_PATH = Path(ceasiompy.__init__.__file__).parents[1]
 

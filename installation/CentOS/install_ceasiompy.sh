ea34e22a0e7d88f9a4113480ca9592c7ac8e4a1b
aidanjungo <aidan@cfse.ch>
2022-07-27 15:41:03 +0200

    Installation script for CentOS (missing SUMO)

---
+++ installation/CentOS/install_ceasiompy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Script to install CEASIOMpy on Ubuntu 20.04 and  Mint 20.3
+
+current_dir="$(pwd)"
+
+sudo dnf install g++
+sudo dnf install libtbb2
+
+cd ../..
+
+conda env create -f environment.yml
+
+source ~/.bashrc
+
+# Activate conda environment to install CEASIOMpy in it
+CONDA_BASE=$(conda info --base)
+source "$CONDA_BASE/etc/profile.d/conda.sh"
+conda activate ceasiompy
+
+pip install -e .
+
+cd "$current_dir"
---
+++ installation/CentOS/install_su2.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Script to install SU2 on Ubuntu 20.04 and  Mint 20.3
+
+su2_version="7.3.0"
+
+current_dir="$(pwd)"
+
+# Get install dir from input if it exists
+if [ $# -gt 0 ]; then
+    install_dir="$1/INSTALLDIR"
+else
+    install_dir="$(pwd)/../../INSTALLDIR"
+fi
+
+echo "Creating install directory..."
+mkdir -p "$install_dir"
+cd "$install_dir"
+
+echo "Downloading SU2..."
+wget https://github.com/su2code/SU2/releases/download/v"$su2_version"/SU2-v"$su2_version"-linux64-mpi.zip
+unzip -d SU2-v"$su2_version"-linux64-mpi SU2-v"$su2_version"-linux64-mpi.zip
+
+echo "Adding path to the .bashrc"
+
+su2_run_path=/"$install_dir"/SU2-v"$su2_version"-linux64-mpi/bin
+su2_home_path=/"$install_dir"/SU2-v"$su2_version"-linux64-mpi
+
+echo \# SU2 Path >> ~/.bashrc
+echo export SU2_RUN=\""$su2_run_path"\" >> ~/.bashrc
+echo export SU2_HOME=\""$su2_home_path"\" >> ~/.bashrc
+echo export PYTHONPATH=\$PYTHONPATH:\$SU2_RUN >> ~/.bashrc
+echo export PATH=\"\$PATH:\$SU2_RUN\" >> ~/.bashrc
+
+echo "Installing MPICH..."
+sudo dnf install -y mpich
+
+cd "$current_dir"

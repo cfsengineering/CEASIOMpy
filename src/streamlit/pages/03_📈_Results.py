700e4133528b4381afc1bcd0086380e172f5cf07
aidanjungo <aidan@cfse.ch>
2022-09-13 14:38:40 +0200

    Add aeromap plot tool

--- src/streamlit/pages/03_📈_Results.py
+++ src/streamlit/pages/03_📈_Results.py
@@ -1,7 +1,11 @@
+from genericpath import isfile
 from pathlib import Path
 
 import ceasiompy.__init__
+import plotly.graph_objects as go
 import streamlit as st
+from cpacspy.cpacspy import CPACS
+from cpacspy.utils import PARAMS_COEFS
 from streamlit_autorefresh import st_autorefresh
 
 CEASIOMPY_PATH = Path(ceasiompy.__init__.__file__).parents[1]
@@ -11,6 +15,24 @@ st.set_page_config(page_title="Results", page_icon="📈")
 st.title("Results")
 
 st.info("The results page is under construction, it can not be use yet!")
+
+
+def get_last_workflow():
+
+    if "workflow" not in st.session_state:
+        st.warning("No workflow to show the result yet.")
+        return
+
+    last_workflow_nb = 0
+
+    for dir in Path(st.session_state.workflow.working_dir).iterdir():
+        if "Workflow_" in str(dir):
+            last_workflow_nb = max(last_workflow_nb, int(str(dir).split("_")[-1]))
+
+    return Path(
+        Path(st.session_state.workflow.working_dir),
+        f"Workflow_{str(last_workflow_nb).rjust(3, '0')}",
+    )
 
 
 def show_logs():
@@ -25,24 +47,127 @@ def show_logs():
     st.text_area("logs (more recent on top)", lines_str, height=300, disabled=True)
 
 
+def show_aeromap():
+
+    st.markdown("#### Aeromap")
+
+    if "cpacs" not in st.session_state:
+        st.warning("No CPACS file has been selected!")
+        return
+
+    last_workflow = get_last_workflow()
+    list_of_files = []
+    for file in last_workflow.iterdir():
+        if file.is_file():
+            list_of_files.append(file.stem)
+    if "ToolOutput" in list_of_files:
+        cpacs = CPACS(Path(last_workflow, "ToolOutput.xml"))
+    else:
+        cpacs = st.session_state.cpacs
+        st.info("No results found, these are aeromaps from the input CPACS file!")
+
+    # Get aeromap(s) to plot from multiselect box
+    aeromap_list = []
+    aeromap_uid_list = cpacs.get_aeromap_uid_list()
+    aeromap_selected = st.multiselect("Select aeromap", aeromap_uid_list)
+    aeromap_list = [cpacs.get_aeromap_by_uid(aeromap_uid) for aeromap_uid in aeromap_selected]
+
+    if not aeromap_list:
+        st.warning("You must select at least one aeromap!")
+        return
+
+    # temp (TODO: could be improve, how to look into all df)
+    df_tmp = aeromap_list[0].df
+
+    # Option choose axis
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        x_axis = st.selectbox("x", PARAMS_COEFS, 3)
+    with col2:
+        y_axis = st.selectbox("y", PARAMS_COEFS, 5)
+
+    # Option filter 1
+    with st.expander("Filter 1"):
+        filt1_col1, filt1_col2 = st.columns([1, 2])
+
+        with filt1_col1:
+            filt1_remaining = [item for item in PARAMS_COEFS if item not in [x_axis, y_axis]]
+            filter1 = st.selectbox("Filter by:", filt1_remaining)
+        with filt1_col2:
+            value_list = df_tmp[filter1].unique()
+            value_selected = st.multiselect("Filter value:", value_list, value_list[0])
+
+    # Option filter 2
+    with st.expander("Filter 2"):
+        filt2_col1, filt2_col2 = st.columns([1, 2])
+        with filt2_col1:
+            filt2_remaining = [
+                item for item in PARAMS_COEFS if item not in [x_axis, y_axis, filter1]
+            ]
+            filter2 = st.selectbox("Filter2 by:", filt2_remaining)
+        with filt2_col2:
+            value_list2 = df_tmp[filter2].unique()
+            value_selected2 = st.multiselect("Filter2 value:", value_list2, value_list2[0])
+
+    # Plot figure
+    fig = go.Figure()
+    for aeromap in aeromap_list:
+
+        if not len(value_selected):
+            value_selected = value_list
+
+        for value in value_selected:
+            if not len(value_selected2):
+                value_selected2 = value_list2
+
+            for value2 in value_selected2:
+                df = aeromap.df[(aeromap.df[filter1] == value) & (aeromap.df[filter2] == value2)]
+                legend = f"{aeromap.uid}<br>{filter1}={value}<br>{filter2}={value2}"
+                fig.add_trace(go.Scatter(x=df[x_axis], y=df[y_axis], name=legend))
+
+    fig.update_traces(
+        mode="markers+lines",
+        hovertemplate="x: %{x:.2f} \ny: %{y:.2f} ",
+    )
+
+    grid_color = "rgb(188,188,188)"
+    axis_color = "rgb(0,0,0)"
+    bg_color = "rgb(255,255,255)"
+
+    fig.update_layout(
+        xaxis=dict(title=x_axis),
+        yaxis=dict(title=y_axis),
+        plot_bgcolor=bg_color,
+    )
+    fig.update_xaxes(
+        showline=True,
+        linewidth=2,
+        linecolor=axis_color,
+        gridcolor=grid_color,
+        zerolinecolor=grid_color,
+    )
+    fig.update_yaxes(
+        showline=True,
+        linewidth=2,
+        linecolor=axis_color,
+        gridcolor=grid_color,
+        zerolinecolor=grid_color,
+    )
+
+    st.plotly_chart(fig)
+
+
 def show_results():
 
     st.markdown("#### Results")
 
-    if "workflow" not in st.session_state:
-        st.warning("No workflow to show the result yet.")
+    last_workflow = get_last_workflow()
+
+    if not last_workflow:
         return
 
-    # TODO: all this part should be improved
-    results_dir = Path(st.session_state.workflow.working_dir)
-    last_workflow = 0
-    for dir in results_dir.iterdir():
-        if "Workflow_" in str(dir):
-            last_workflow = max(last_workflow, int(str(dir).split("_")[-1]))
-
-    results_dir = Path(results_dir, f"Workflow_{str(last_workflow).rjust(3, '0')}", "Results")
-    st.text(f"The Results dir is: {str(results_dir)}")
-    for dir in results_dir.iterdir():
+    for dir in Path(last_workflow, "Results").iterdir():
         if dir.is_dir():
             with st.expander(dir.name, expanded=False):
                 st.text("")
@@ -51,6 +176,7 @@ def show_results():
 
 
 show_logs()
+show_aeromap()
 show_results()
 
 st_autorefresh(interval=1000, limit=100, key="auto_refresh")

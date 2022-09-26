bc04ddbcdb08bf3ac859b6ff18612523beb283e6
aidanjungo <aidan@cfse.ch>
2022-09-26 10:36:45 +0200

    test stl preview (wip)

--- src/streamlit/pages/04_📈_Results.py
+++ src/streamlit/pages/04_📈_Results.py
@@ -175,8 +175,56 @@ def show_results():
                     else:
                         st.markdown(f"This file cannot be shown: {file}")
 
+    st.markdown("## TEST")
+
+    import pyvista as pv
+    import io
+    import tempfile
+    import streamlit.components.v1 as components
+
+    ## Using pythreejs as pyvista backend
+    pv.set_jupyter_backend("pythreejs")
+
+    ## Upload a pyvista file
+    uploadedFile = st.file_uploader("Upload a STL:", ["stl"], False)
+
+    ## Streamlit layout
+    st.sidebar.title("STL viewer")
+
+    if uploadedFile:
+
+        ## Color panel
+        col1, col2 = st.sidebar.columns(2)
+        color_stl = col1.color_picker("Element", "#0BD88D")
+        color_bkg = col2.color_picker("Background", "#FFFFFF")
+
+        ## Initialize pyvista reader and plotter
+        plotter = pv.Plotter(border=False, window_size=[500, 400])
+        plotter.background_color = color_bkg
+
+        ## Create a tempfile to keep the uploaded file as pyvista's API
+        ## only supports file paths but not buffers
+        with tempfile.NamedTemporaryFile(suffix="_streamlit") as f:
+            f.write(uploadedFile.getbuffer())
+            reader = pv.STLReader(f.name)
+
+            ## Read data and send to plotter
+            mesh = reader.read()
+            plotter.add_mesh(mesh, color=color_stl)
+
+            ## Export to a pythreejs HTML
+            model_html = io.StringIO()
+            plotter.export_html(model_html, backend="pythreejs")
+
+            ## Show in webpage
+            components.html(model_html.getvalue(), height=400, width=500, scrolling=False)
+
+            ## Print HTML
+            with st.expander("Code"):
+                st.code(model_html.getvalue(), language="cshtml")
+
 
 show_aeromap()
 show_results()
 
-st_autorefresh(interval=3000, limit=10000, key="auto_refresh")
+# st_autorefresh(interval=3000, limit=10000, key="auto_refresh")

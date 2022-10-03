6c81b98d26745f64283343e316aca116ce8cfcea
aidanjungo <aidan@cfse.ch>
2022-10-03 14:07:41 +0200

    Add test

--- ceasiompy/CPACS2SUMO/tests/test_cpacs2sumo.py
+++ ceasiompy/CPACS2SUMO/tests/test_cpacs2sumo.py
@@ -21,6 +21,8 @@ TODO:
 # =================================================================================================
 
 import pytest
+from pytest import approx
+from ceasiompy.CPACS2SUMO.func.cst2coord import CST_shape
 
 # =================================================================================================
 #   CLASSES
@@ -36,6 +38,43 @@ import pytest
 def test_cpacs2sumo():
 
     pass
+
+
+def test_cst2coord():
+
+    # Simple 4 points profile with default values
+    airfoil_CST = CST_shape(N=4)
+    airfoil_CST.airfoil_coor()
+
+    assert airfoil_CST.x_list == approx([1.0, 0.5, 0.0, 0.5], rel=1e-5)
+    assert airfoil_CST.y_list == approx([0.0, -0.35355, 0.0, 0.35355], rel=1e-3)
+
+    # More complex profile
+
+    wu = [0.2, 0.45, -0.12, 1.0, -0.473528, 0.95, 0.14, 0.38, 0.11, 0.38]
+    wl = [-0.13, 0.044, -0.38, 0.43, -0.74, 0.54, -0.51, 0.10, -0.076, 0.062]
+    dz = 0.00
+    N = 10
+
+    airfoil_CST = CST_shape(wl, wu, dz, N)
+    airfoil_CST.airfoil_coor()
+
+    x_true_values = [1.0, 0.9045, 0.6545, 0.3454, 0.0954, 0.0, 0.0954, 0.3454, 0.6545, 0.9045]
+    y_true_values = [
+        0.0,
+        -0.0004864,
+        -0.0216,
+        -0.03189,
+        -0.02366,
+        0.0,
+        0.07616,
+        0.12024,
+        0.09293,
+        0.02447,
+    ]
+
+    assert airfoil_CST.x_list == approx(x_true_values, rel=1e-3)
+    assert airfoil_CST.y_list == approx(y_true_values, rel=1e-3)
 
 
 # =================================================================================================

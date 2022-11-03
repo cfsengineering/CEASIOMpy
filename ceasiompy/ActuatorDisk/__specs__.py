8441aa9b07b89009a3eea5d0184f4317ed58df27
GBenedett <giacomo.benedetti28@gmail.com>
2022-11-03 14:34:41 +0100

    add specs file

--- ceasiompy/ActuatorDisk/__specs__.py
+++ ceasiompy/ActuatorDisk/__specs__.py
@@ -1,48 +1,142 @@
+from pathlib import Path
+
+from ceasiompy.utils.commonxpath import (AEROPERFORMANCE_XPATH,
+                                         CEASIOMPY_XPATH,
+                                         RANGE_XPATH, 
+                                         PROP_XPATH, 
+                                         GEOM_XPATH,
+                                         )
 from ceasiompy.utils.moduleinterfaces import CPACSInOut
-from ceasiompy.utils.commonxpath import CEASIOMPY_XPATH, FUSELAGES_XPATH
 
 # ===== Module Status =====
 # True if the module is active
 # False if the module is disabled (not working or not ready)
-module_status = False  # Because it is just an example not a real module
+module_status = True
+
+# ===== Results directory path =====
+
+RESULTS_DIR = Path("Results", "AD")
 
 # ===== CPACS inputs and outputs =====
 
 cpacs_inout = CPACSInOut()
 
-include_gui = False
-
 # ----- Input -----
 
 cpacs_inout.add_input(
-    var_name="test",
-    var_type=str,
-    default_value="This is a test",
-    unit=None,
-    descr="This is a test of description",
-    xpath=CEASIOMPY_XPATH + "/test/myTest",
-    gui=include_gui,
-    gui_name="My test",
-    gui_group="Group Test",
+    var_name="cruise_mach",
+    var_type=float,
+    default_value=0.4,
+    unit="1",
+    descr="Aircraft cruise Mach number",
+    xpath=RANGE_XPATH + "/cruiseMach",
+    gui=False,
+    gui_name="Cruise Mach",
+    gui_group="If fixed CL",
 )
 
 cpacs_inout.add_input(
-    var_name="other_var",
-    var_type=list,
-    default_value=[2, 33, 444],
-    unit="[unit]",
-    xpath=CEASIOMPY_XPATH + "/test/myList",
-    gui=include_gui,
-    gui_name="Choice",
-    gui_group="My Selection",
+    var_name="cruise_alt",
+    var_type=float,
+    default_value=9000.0,
+    unit="m",
+    descr="Aircraft cruise altitude",
+    xpath=RANGE_XPATH + "/cruiseAltitude",
+    gui=False,
+    gui_name="Cruise Altitude",
+    gui_group="If fixed CL",
+)
+
+cpacs_inout.add_input(
+    var_name="station",
+    var_type=float,
+    default_value=20,
+    unit=None,
+    descr="Number of elements for blade discretization",
+    xpath=PROP_XPATH + "/propeller/blade/discretization",
+    gui=True,
+    gui_name="Blade discretization",
+    gui_group="Optimal prop settings",
+)
+
+cpacs_inout.add_input(
+    var_name="radius",
+    var_type=float,
+    default_value=2,
+    unit="m",
+    descr="Propeller radius",
+    xpath=PROP_XPATH + "/propeller/blade/discretization",
+    gui=True,
+    gui_name="Thrust setting",
+    gui_group="Optimal prop settings",
+)
+
+cpacs_inout.add_input(
+    var_name="thrust",
+    var_type=float,
+    default_value=6500,
+    unit="N",
+    descr="Aircraft thrust",
+    xpath=PROP_XPATH + "propeller/thrust",
+    gui=True,
+    gui_name="Thrust setting",
+    gui_group="Optimal prop settings",
+)
+
+cpacs_inout.add_input(
+    var_name="n",
+    var_type=float,
+    default_value=2000,
+    unit="1/s",
+    descr="Propeller rotational velocity",
+    xpath=PROP_XPATH + "propeller/rotational_velocity",
+    gui=True,
+    gui_name="Rotational velocity setting",
+    gui_group="Optimal prop settings",
+)
+
+cpacs_inout.add_input(
+    var_name="no_prandtl_correction",
+    var_type=bool,
+    default_value=False,
+    unit=None,
+    descr="No tip loss correction",
+    xpath=PROP_XPATH + "propeller/blade/loss",
+    gui=True,
+    gui_name="No tip loss correction",
+    gui_group="Optimal prop settings",
+)
+
+cpacs_inout.add_input(
+    var_name="prandtl_correction",
+    var_type=bool,
+    default_value=True,
+    unit=None,
+    descr="Enable the tip loss correction of Prandtl",
+    xpath=PROP_XPATH + "propeller/blade/loss",
+    gui=True,
+    gui_name="Tip loss correction",
+    gui_group="Optimal prop settings",
+)
+
+cpacs_inout.add_input(
+    var_name="blade_numbers",
+    var_type=int,
+    default_value=2,
+    unit=None,
+    descr="Number of propeller blades",
+    xpath=PROP_XPATH + "propeller/blade",
+    gui=True,
+    gui_name="Correction",
+    gui_group="Optimal prop settings",
 )
 
 # ----- Output -----
 
 cpacs_inout.add_output(
-    var_name="output",
+    var_name="thrust_coefficient_distribution",
     default_value=None,
     unit="1",
-    descr="Description of the output",
-    xpath=CEASIOMPY_XPATH + "/test/myOutput",
+    descr="Distribution of thrust coefficient along the radius",
+    xpath=AEROPERFORMANCE_XPATH + "/aeroMap/aeroPerformanceMap",
 )

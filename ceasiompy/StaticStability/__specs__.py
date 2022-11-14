3c2b912c542ef7dd29b0f5a7113cd314a5b233a5
aidanjungo <aidan@cfse.ch>
2022-11-14 15:56:05 +0100

    Activate module with dummy results

--- ceasiompy/StaticStability/__specs__.py
+++ ceasiompy/StaticStability/__specs__.py
@@ -3,13 +3,13 @@
 
 from pathlib import Path
 from ceasiompy.utils.moduleinterfaces import CPACSInOut
-from utils.commonxpath import STABILITY_XPATH
+from ceasiompy.utils.commonxpath import STABILITY_XPATH
 
 
 # ===== Module Status =====
 # True if the module is active
 # False if the module is disabled (not working or not ready)
-module_status = False
+module_status = True
 
 # ===== Results directory path =====
 
@@ -24,15 +24,15 @@ cpacs_inout = CPACSInOut()
 # ===== Input =====
 
 cpacs_inout.add_input(
-    var_name='aeromap_to_analyze',
+    var_name="aeromap_to_analyze",
     var_type=list,
     default_value=None,
     unit=None,
     descr="Name of the aero map to evaluate",
     xpath=STABILITY_XPATH + "/aeroMapToAnalyze",
     gui=True,
-    gui_name='__AEROMAP_SELECTION',
-    gui_group='Inputs',
+    gui_name="__AEROMAP_SELECTION",
+    gui_group="Inputs",
 )
 
 
--- ceasiompy/StaticStability/staticstability.py
+++ ceasiompy/StaticStability/staticstability.py
@@ -20,7 +20,13 @@ TODO:
 
 from pathlib import Path
 from cpacspy.cpacspy import CPACS
-
+from cpacspy.cpacsfunctions import (
+    add_string_vector,
+    create_branch,
+    get_string_vector,
+)
+from ceasiompy.utils.ceasiompyutils import get_results_directory
+from markdownpy.markdownpy import MarkdownDoc
 from ceasiompy.utils.ceasiomlogger import get_logger
 from ceasiompy.utils.commonxpath import CEASIOMPY_XPATH, STABILITY_XPATH
 from ceasiompy.utils.moduleinterfaces import (
@@ -29,7 +35,7 @@ from ceasiompy.utils.moduleinterfaces import (
     get_tooloutput_file_path,
 )
 
-log = get_logger(__file__.split(".")[0])
+log = get_logger()
 
 MODULE_DIR = Path(__file__).parent
 MODULE_NAME = MODULE_DIR.name
@@ -52,11 +58,28 @@ def static_stability_analysis(cpacs_path, cpacs_out_path):
 
     cpacs = CPACS(cpacs_path)
 
-    aeromap_to_analyze = STABILITY_XPATH + "/aeroMapToAnalyze"
+    results_dir = get_results_directory("StaticStability")
+    md = MarkdownDoc(Path(results_dir, "CL_Calculator.md"))
+    md.h2("Static Stability")
 
-    print(aeromap_to_analyze)
+    aeromap_to_analyze_xpath = STABILITY_XPATH + "/aeroMapToAnalyze"
+
+    aeromap_uid_list = []
+
+    try:
+        aeromap_uid_list = get_string_vector(cpacs.tixi, aeromap_to_analyze_xpath)
+    except ValueError:  # if aeroMapToPlot is not define, select all aeromaps
+        aeromap_uid_list = cpacs.get_aeromap_uid_list()
+        create_branch(cpacs.tixi, aeromap_to_analyze_xpath)
+        add_string_vector(cpacs.tixi, aeromap_to_analyze_xpath, aeromap_uid_list)
+
+    for aeromap_uid in aeromap_uid_list:
+        md.h3(aeromap_uid)
+        md.p(f"Test {aeromap_uid}!!!")
+        aeromap = cpacs.get_aeromap_by_uid(aeromap_uid)
 
     cpacs.save_cpacs(cpacs_out_path, overwrite=True)
+    md.save()
 
 
 # =================================================================================================
@@ -68,7 +91,7 @@ def main(cpacs_path, cpacs_out_path):
 
     log.info("----- Start of " + MODULE_NAME + " -----")
 
-    check_cpacs_input_requirements(cpacs_path)
+    # check_cpacs_input_requirements(cpacs_path)
 
     static_stability_analysis(cpacs_path, cpacs_out_path)
 

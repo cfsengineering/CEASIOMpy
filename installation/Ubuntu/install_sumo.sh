d666561466fcc99d8814a981430bd25fd0c68502
aidanjungo <aidan@cfse.ch>
2022-11-22 15:34:53 +0100

    http to https

--- installation/Ubuntu/install_sumo.sh
+++ installation/Ubuntu/install_sumo.sh
@@ -25,8 +25,8 @@ echo "Adding missing librairies..."
 
 echo "--> libgortran3"
 if ! grep "ethz.ch/ubuntu/" /etc/apt/sources.list ; then
-    sudo  -- bash -c 'echo "deb http://ubuntu.ethz.ch/ubuntu/ bionic universe" >> /etc/apt/sources.list'
-    sudo  -- bash -c 'echo "deb http://ubuntu.ethz.ch/ubuntu/ bionic-updates universe" >> /etc/apt/sources.list'
+    sudo  -- bash -c 'echo "deb https://ubuntu.ethz.ch/ubuntu/ bionic universe" >> /etc/apt/sources.list'
+    sudo  -- bash -c 'echo "deb https://ubuntu.ethz.ch/ubuntu/ bionic-updates universe" >> /etc/apt/sources.list'
 fi
 sudo apt-get update -y
 sudo apt-get install -y libgfortran3

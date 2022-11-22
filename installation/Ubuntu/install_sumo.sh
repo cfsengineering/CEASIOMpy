82d32eac44182534ee855aa9ec63ded203bd5f53
aidanjungo <aidan@cfse.ch>
2022-11-22 16:12:18 +0100

    test without https

--- installation/Ubuntu/install_sumo.sh
+++ installation/Ubuntu/install_sumo.sh
@@ -27,8 +27,8 @@ echo "--> libgortran3"
 if ! grep "ethz.ch/ubuntu/" /etc/apt/sources.list ; then
     sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32
     sudo apt update
-    sudo  -- bash -c 'echo "deb https://ubuntu.ethz.ch/ubuntu/ bionic universe" >> /etc/apt/sources.list'
-    sudo  -- bash -c 'echo "deb https://ubuntu.ethz.ch/ubuntu/ bionic-updates universe" >> /etc/apt/sources.list'
+    sudo  -- bash -c 'echo "deb http://ubuntu.ethz.ch/ubuntu/ bionic universe" >> /etc/apt/sources.list'
+    sudo  -- bash -c 'echo "deb http://ubuntu.ethz.ch/ubuntu/ bionic-updates universe" >> /etc/apt/sources.list'
 fi
 sudo apt-get update -y
 sudo apt-get install -y libgfortran3

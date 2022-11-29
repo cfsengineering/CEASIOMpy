ab0ad8e1ca07fc8e2a2e42eb1de4dab79f4d8f3e
GBenedett <giacomo.benedetti28@gmail.com>
2022-11-29 16:27:13 +0100

    test radius changed

--- ceasiompy/ActuatorDisk/tests/test_actuatordisk.py
+++ ceasiompy/ActuatorDisk/tests/test_actuatordisk.py
@@ -90,17 +90,17 @@ def test_check_output():
 
     """
     input_values = {
-        "test1": [[20, 0.5, 1.5, 0.2, 1.5, 150, True, 2], [0.5, 0.975, 45000, 0.7688]],
-        "test2": [[20, 0.8, 1.5, 0.15, 2, 190, True, 3], [0.8, 2.217, 64980, 0.7215]],
-        "test3": [[20, 1, 1.2, 0.1, 1.8, 180, False, 3], [1, 2.5304, 57600, 0.7113]],
-        "test4": [[20, 1.2, 1.4, 0.1, 1.6, 140, True, 2], [1.2, 3.6097, 72030, 0.5318]],
-        "test5": [[20, 1.5, 2, 0.2, 1.8, 190, True, 8], [1.5, 5.9200, 267407.41, 0.4560]],
-        "test6": [[20, 0.2, 1.4, 0.1, 1.4, 130, True, 2], [0.2, 0.3148, 13520, 0.8892]],
-        "test7": [[20, 0.15, 1.4, 0.1, 1.3, 130, True, 2], [0.15, 0.2149, 11760, 0.9071]],
-        "test8": [[20, 1.3, 1.7, 0.5107, 1.7, 160, False, 6], [1.3, 3.5762, 133120, 0.6179]],
+        "test1": [[20, 0.5, 1.5, 0.2, 1.5, 150, True, 2, 33], [0.5, 0.975, 45000, 0.7688]],
+        "test2": [[20, 0.8, 1.5, 0.15, 2, 190, True, 3, 33], [0.8, 2.217, 64980, 0.7215]],
+        "test3": [[20, 1, 1.2, 0.1, 1.8, 180, False, 3, 33], [1, 2.5304, 57600, 0.7113]],
+        "test4": [[20, 1.2, 1.4, 0.1, 1.6, 140, True, 2, 33], [1.2, 3.6097, 72030, 0.5318]],
+        "test5": [[20, 1.5, 2, 0.2, 1.8, 190, True, 8, 33], [1.5, 5.9200, 267407.41, 0.4560]],
+        "test6": [[20, 0.2, 1.4, 0.1, 1.4, 130, True, 2, 33], [0.2, 0.3148, 13520, 0.8892]],
+        "test7": [[20, 0.15, 1.4, 0.1, 1.3, 130, True, 2, 33], [0.15, 0.2149, 11760, 0.9071]],
+        "test8": [[20, 1.3, 1.7, 0.5107, 1.7, 160, False, 6, 33], [1.3, 3.5762, 133120, 0.6179]],
         "test9": [
-            [37, 0.15, 2.5146, 0.2, 2.81487, 190.5488, False, 6],
-            [0.15, 0.4368, 17385.4054, 0.9673],
+            [37, 0.15, 2.5146, 0.2, 2.81487, 190.5488, False, 6, 33],
+            [0.15, 0.4368, 17385.4054, 0.9673, 33],
         ],
     }
 
@@ -135,7 +135,7 @@ def test_file_exist():
     if actuator_disk_dat_path.exists():
         actuator_disk_dat_path.unlink()
 
-    thrust_calculator(20, 0.5, 1.5, 0.15, 1.5, 150, True, 2)
+    thrust_calculator(20, 0.5, 1.5, 0.15, 1.5, 150, True, 2, 33)
 
     assert actuator_disk_dat_path.exists()
 
@@ -164,7 +164,7 @@ def test_plot_exist():
         optimal_rotational_interference_factor,
         prandtl,
         correction_function,
-    ) = thrust_calculator(37, 0.15, 2.5146, 0.2, 2.81487, 190.5488, True, 6)
+    ) = thrust_calculator(37, 0.15, 2.5146, 0.2, 2.81487, 190.5488, True, 6, 33)
 
     function_plot(
         r,
@@ -186,9 +186,9 @@ def test_plot_exist():
 def test_adimentional_radius():
     with pytest.raises(ValueError):
         adimensional_radius(1, 1.1, 11)
-    np.testing.assert_almost_equal(adimensional_radius(1, 0.1, 11), (np.arange(0.1, 1.1, 0.1)))
-    np.testing.assert_almost_equal(adimensional_radius(1, 0.5, 11), (np.arange(0.5, 1.05, 0.1)))
-    np.testing.assert_almost_equal(adimensional_radius(1, 0.11, 11), (np.arange(0.2, 1.05, 0.1)))
+    np.testing.assert_almost_equal(adimensional_radius(1, 0.1, 10), (np.arange(0.1, 1.05, 0.1)))
+    np.testing.assert_almost_equal(adimensional_radius(1, 0.5, 10), (np.arange(0.5, 1.05, 0.1)))
+    np.testing.assert_almost_equal(adimensional_radius(1, 0.11, 10), (np.arange(0.2, 1.05, 0.1)))
 
 
 # =================================================================================================

b7519ea98e7f534499454ea8233af7ae14b0b339
GBenedett <giacomo.benedetti28@gmail.com>
2022-11-29 16:41:48 +0100

    test value added, removed test 9

--- ceasiompy/ActuatorDisk/tests/test_actuatordisk.py
+++ ceasiompy/ActuatorDisk/tests/test_actuatordisk.py
@@ -84,24 +84,20 @@ def test_prandtl_corr():
 def test_check_output():
     """Test function which made different test on thrust_coefficient function, the test function
     recive a vector with input parameter [stations, total_thrust_coefficient, radius,
-    hub radius, advanced_ratio, free_stream_velocity, prandtl, blades_number] the function will
-    give an output file to compare with given result vector
+    hub radius, advanced_ratio, free_stream_velocity, prandtl, blades_number, rotational_velocity]
+    the function will give an output file to compare with given result vector
     [renard_thrust_coeff, power_coeff, thrust_over_density, efficiency]
 
     """
     input_values = {
-        "test1": [[20, 0.5, 1.5, 0.2, 1.5, 150, True, 2, 33], [0.5, 0.975, 45000, 0.7688]],
-        "test2": [[20, 0.8, 1.5, 0.15, 2, 190, True, 3, 33], [0.8, 2.217, 64980, 0.7215]],
-        "test3": [[20, 1, 1.2, 0.1, 1.8, 180, False, 3, 33], [1, 2.5304, 57600, 0.7113]],
-        "test4": [[20, 1.2, 1.4, 0.1, 1.6, 140, True, 2, 33], [1.2, 3.6097, 72030, 0.5318]],
-        "test5": [[20, 1.5, 2, 0.2, 1.8, 190, True, 8, 33], [1.5, 5.9200, 267407.41, 0.4560]],
-        "test6": [[20, 0.2, 1.4, 0.1, 1.4, 130, True, 2, 33], [0.2, 0.3148, 13520, 0.8892]],
-        "test7": [[20, 0.15, 1.4, 0.1, 1.3, 130, True, 2, 33], [0.15, 0.2149, 11760, 0.9071]],
-        "test8": [[20, 1.3, 1.7, 0.5107, 1.7, 160, False, 6, 33], [1.3, 3.5762, 133120, 0.6179]],
-        "test9": [
-            [37, 0.15, 2.5146, 0.2, 2.81487, 190.5488, False, 6, 33],
-            [0.15, 0.4368, 17385.4054, 0.9673, 33],
-        ],
+        "test1": [[20, 0.5, 1.5, 0.2, 1.5, 150, True, 2, 33], [0.5, 0.994, 45000, 0.754]],
+        "test2": [[20, 0.8, 1.5, 0.15, 2, 190, True, 3, 33], [0.8, 1.890, 64980, 0.846]],
+        "test3": [[20, 1, 1.2, 0.1, 1.8, 180, False, 3, 33], [1, 5.796, 57600, 0.3105]],
+        "test4": [[20, 1.2, 1.4, 0.1, 1.6, 140, True, 2, 33], [1.2, 2.792, 72030, 0.687]],
+        "test5": [[20, 1.5, 2, 0.2, 1.8, 190, True, 8, 33], [1.5, 2.189, 267407.41, 1.233]],
+        "test6": [[20, 0.2, 1.4, 0.1, 1.4, 130, True, 2, 33], [0.2, 0.3178, 13520, 0.8809]],
+        "test7": [[20, 0.15, 1.4, 0.1, 1.3, 130, True, 2, 33], [0.15, 0.2725, 11760, 0.7153]],
+        "test8": [[20, 1.3, 1.7, 0.5107, 1.7, 160, False, 6, 22], [1.3, 9.8345, 133120, 0.2247]],
     }
 
     for values in input_values.values():

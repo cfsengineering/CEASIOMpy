3c7053f82e8eb424f538510ba8ad172f317e0d7c
aidanjungo <aidan@cfse.ch>
2022-12-02 10:53:12 +0100

    Remove unused old fsi function

--- ceasiompy/SU2Run/su2run.py
+++ ceasiompy/SU2Run/su2run.py
@@ -98,55 +98,6 @@ def run_SU2_multi(wkdir, nb_proc):
             )
 
 
-# TODO: Refactor this function
-# TODO: The deformation part should be moved to SU2MeshDef module
-def run_SU2_fsi(config_path, wkdir, nb_proc):
-    """Function to run a SU2 calculation for FSI .
-
-    Function 'run_SU2_fsi' deforms an element of the mesh (e.g. wing) from
-    point file 'disp.dat' given by a structural model and then runs a SU2
-    calculation (SU2_CFD then SU2_SOL) with the given config_path. Finally a
-    load file is saved, to be send to the structural model.
-
-    Args:
-        config_path (str): Path to the configuration file
-        wkdir (str): Path to the working directory
-
-    """
-
-    raise NotImplementedError
-
-    # from ceasiompy.utils.configfiles import ConfigFile
-    # from ceasiompy.SU2Run.func.extractloads import extract_loads
-
-    # if not wkdir.exists():
-    #     raise OSError(f"The working directory : {wkdir} does not exit!")
-
-    # # Modify config file for SU2_DEF
-    # config_def_path = Path(wkdir, "ConfigDEF.cfg")
-    # cfg_def = ConfigFile(config_path)
-
-    # cfg_def["DV_KIND"] = "SURFACE_FILE"
-    # cfg_def["DV_MARKER"] = "Wing"
-    # cfg_def["DV_FILENAME"] = "disp.dat"  # TODO: Should be a constant or find in CPACS ?
-    # # TODO: Do we need that? if yes, find 'WING' in CPACS
-    # cfg_def["DV_PARAM"] = ["WING", "0", "0", "1", "0.0", "0.0", "1.0"]
-    # cfg_def["DV_VALUE"] = 0.01
-    # cfg_def.write_file(config_def_path, overwrite=True)
-
-    # # Modify config file for SU2_CFD
-    # config_cfd_path = Path(wkdir, CONFIG_CFD_NAME)
-    # cfg_cfd = ConfigFile(config_path)
-    # cfg_cfd["MESH_FILENAME"] = "mesh_out.su2"
-    # cfg_cfd.write_file(config_cfd_path, overwrite=True)
-
-    # # run_soft("SU2_DEF", config_def_path, wkdir, nb_proc)
-    # # run_soft("SU2_CFD", config_cfd_path, wkdir, nb_proc)
-    # # run_soft("SU2_SOL", config_cfd_path, wkdir, nb_proc)
-
-    # extract_loads(wkdir)
-
-
 # =================================================================================================
 #    MAIN
 # =================================================================================================

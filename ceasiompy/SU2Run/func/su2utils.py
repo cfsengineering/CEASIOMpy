294c12c9a41bdd13fa94f7cb207cdacbcedbc990
aidanjungo <aidan@cfse.ch>
2022-12-02 11:32:59 +0100

    minor

--- ceasiompy/SU2Run/func/su2utils.py
+++ ceasiompy/SU2Run/func/su2utils.py
@@ -3,7 +3,7 @@ CEASIOMpy: Conceptual Aircraft Design Software
 
 Developed for CFS ENGINEERING, 1015 Lausanne, Switzerland
 
-Functions to manipulate SU2 Configuration and results.
+Functions to manipulate SU2 configuration and results.
 
 Python version: >=3.8
 
@@ -11,7 +11,6 @@ Python version: >=3.8
 | Creation: 2019-09-30
 
 TODO:
-
 
 """
 
@@ -131,7 +130,7 @@ def get_su2_version():
     with open(su2py_path, "r") as f:
         for line in f.readlines():
 
-            if not "version" in line:
+            if "version" not in line:
                 continue
 
             version = re.search(r"version\s*([\d.]+)", line).group(1)
@@ -194,7 +193,7 @@ def get_su2_aerocoefs(force_file):
                 cd = float(line.split(":")[1].split("|")[0])
             if "Total CSF:" in line:
                 cs = float(line.split(":")[1].split("|")[0])
-            # TODO: Check which axis name corespond to that: cml, cmd, cms
+            # TODO: Check which axis name correspond to that: cml, cmd, cms
             if "Total CMx:" in line:
                 cmd = float(line.split(":")[1].split("|")[0])
             if "Total CMy:" in line:
@@ -291,5 +290,3 @@ def get_wetted_area(su2_logfile):
 if __name__ == "__main__":
 
     print("Nothing to execute!")
-    print("You can use this module by importing for example:")
-    print("from ceasiompy.SU2Run.func.su2meshutils import get_mesh_markers")

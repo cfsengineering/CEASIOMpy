6540d5fd45ebef6b393e3ded98b2053167d5276e
aidanjungo <aidan@cfse.ch>
2022-12-02 11:19:25 +0100

    Remove comment and simplification

--- ceasiompy/SU2Run/func/su2config.py
+++ ceasiompy/SU2Run/func/su2config.py
@@ -126,29 +126,21 @@ def generate_su2_cfd_config(cpacs_path, cpacs_out_path, wkdir):
     else:  # if fixed_cl == 'YES':
         log.info("Configuration file for fixed CL calculation will be created.")
 
-        # Create a new aeroMap
         fixed_cl_aeromap = cpacs.create_aeromap("aeroMap_fixedCL_SU2")
         fixed_cl_aeromap.description = f"AeroMap created for SU2 fixed CL value of {target_cl}"
 
-        # Get cruise mach and altitude
-        cruise_mach_xpath = RANGE_XPATH + "/cruiseMach"
-        mach = get_value_or_default(cpacs.tixi, cruise_mach_xpath, 0.78)
-        cruise_alt_xpath = RANGE_XPATH + "/cruiseAltitude"
-        alt = get_value_or_default(cpacs.tixi, cruise_alt_xpath, 12000)
+        mach = get_value_or_default(cpacs.tixi, RANGE_XPATH + "/cruiseMach", 0.78)
+        alt = get_value_or_default(cpacs.tixi, RANGE_XPATH + "/cruiseAltitude", 12000)
 
-        # Add new parameters to the aeroMap and save it
         fixed_cl_aeromap.add_row(alt=alt, mach=mach, aos=0.0, aoa=0.0)
         fixed_cl_aeromap.save()
 
-        # Parameter lists
         alt_list = [alt]
         mach_list = [mach]
         aoa_list = [0.0]
         aos_list = [0.0]
 
-    # Get and modify the default configuration file
-    su2_congig_template_path = get_su2_config_template()
-    cfg = ConfigFile(su2_congig_template_path)
+    cfg = ConfigFile(get_su2_config_template())
 
     # Check if symmetry plane is defined (Default: False)
     sym_factor = 1.0

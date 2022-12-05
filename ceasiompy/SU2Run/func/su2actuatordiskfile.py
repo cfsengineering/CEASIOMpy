43b39998bee88a9aa7ef92dbd02519b7032b5e73
aidanjungo <aidan@cfse.ch>
2022-12-05 11:19:52 +0100

    write_actuator_disk_data function and test

--- ceasiompy/SU2Run/func/su2actuatordiskfile.py
+++ ceasiompy/SU2Run/func/su2actuatordiskfile.py
@@ -65,7 +65,18 @@ def write_header(file):
     return file
 
 
-def write_actuator_disk_data(file, inlet_marker, outlet_marker, center, axis, radius):
+def write_actuator_disk_data(
+    file,
+    inlet_marker,
+    outlet_marker,
+    center,
+    axis,
+    radius,
+    advance_ratio,
+    radial_stations,
+    radial_thrust_coefs,
+    radial_power_coefs,
+):
     """Function to create a ActuatorDisk.dat file which is used to when rotor/propeller are define
     in the CPACS file.
 
@@ -76,67 +87,27 @@ def write_actuator_disk_data(file, inlet_marker, outlet_marker, center, axis, ra
         center (tuple): Center of the actuator disk (x,y,z)
         axis (tuple): Axis of the actuator disk (x,y,z)
         radius (float): Radius of the actuator disk
-
+        advance_ratio (float): Advance ratio
+        radial_stations (np.array): adimensional radius along the blade. Multiply by radius to
+                                       get the real radius value
+        radial_thrust_coefs (np.array): Thrust coefficient at each radial_stations
+        radial_power_coefs (np.array): Power coefficient at each radial_stations
     """
 
-    # TODO: add Ct for this engine
+    total_thrust_coefficient = sum(radial_thrust_coefs * (radial_stations[1] - radial_stations[0]))
+    file.write(f"# Total thurst coefficient= {total_thrust_coefficient:.5f}\n")
 
     file.write(f"MARKER_ACTDISK= {inlet_marker} {outlet_marker}\n")
     file.write(f"CENTER= {center[0]} {center[1]} {center[2]}\n")
     file.write(f"AXIS= {axis[0]} {axis[1]} {axis[2]}\n")
     file.write(f"RADIUS= {radius}\n")
-
-    # These value must be specific to a propeller, but how to genereate them?
-    file.write("ADV_RATIO= 2.0\n")
-    file.write("NROW= 47\n")
+    file.write(f"ADV_RATIO= {advance_ratio:.5f}\n")
+    file.write(f"NROW= {len(radial_stations)}\n")
     file.write("# rs=r/R    dCT/drs     dCP/drs     dCR/drs\n")
-    file.write("0.0031   0.20066   00.890674         0.0\n")
-    file.write("0.0235   0.20066   00.890674         0.0\n")
-    file.write("0.0439   0.20066   00.890674         0.0\n")
-    file.write("0.0644   0.20066   00.890674         0.0\n")
-    file.write("0.0848   0.20066   00.890674         0.0\n")
-    file.write("0.1031   0.20066   00.890674         0.0\n")
-    file.write("0.1235   0.20066   00.890674         0.0\n")
-    file.write("0.1439   0.20066   00.890674         0.0\n")
-    file.write("0.1644   0.20066   00.890674         0.0\n")
-    file.write("0.1848   0.20066   00.890674         0.0\n")
-    file.write("0.2031   0.20066   00.890674         0.0\n")
-    file.write("0.2235   0.19963   00.932674         0.0\n")
-    file.write("0.2439   0.21707   00.982980         0.0\n")
-    file.write("0.2644   0.24667   01.064153         0.0\n")
-    file.write("0.2848   0.29147   01.189045         0.0\n")
-    file.write("0.3257   0.43674   01.588513         0.0\n")
-    file.write("0.3461   0.53380   01.849900         0.0\n")
-    file.write("0.3665   0.64327   02.145367         0.0\n")
-    file.write("0.3870   0.76521   02.471873         0.0\n")
-    file.write("0.4278   1.03679   03.203392         0.0\n")
-    file.write("0.4483   1.18918   03.609085         0.0\n")
-    file.write("0.4687   1.35619   04.051864         0.0\n")
-    file.write("0.4891   1.52986   04.518863         0.0\n")
-    file.write("0.5096   1.71453   05.011266         0.0\n")
-    file.write("0.5300   1.90755   05.528521         0.0\n")
-    file.write("0.5504   2.11062   06.072281         0.0\n")
-    file.write("0.5709   2.31313   06.620508         0.0\n")
-    file.write("0.5913   2.51252   07.161404         0.0\n")
-    file.write("0.6117   2.71376   07.700722         0.0\n")
-    file.write("0.6322   2.90980   08.219708         0.0\n")
-    file.write("0.6526   3.09848   08.715231         0.0\n")
-    file.write("0.6730   3.28502   09.202496         0.0\n")
-    file.write("0.6935   3.46774   09.681596         0.0\n")
-    file.write("0.7139   3.64895   10.156277         0.0\n")
-    file.write("0.7343   3.81991   10.603740         0.0\n")
-    file.write("0.7548   3.98417   11.036331         0.0\n")
-    file.write("0.7752   4.13550   11.442054         0.0\n")
-    file.write("0.7956   4.27447   11.820164         0.0\n")
-    file.write("0.8161   4.40093   12.163819         0.0\n")
-    file.write("0.8365   4.51007   12.453084         0.0\n")
-    file.write("0.8569   4.60535   12.682212         0.0\n")
-    file.write("0.8774   4.67765   12.823500         0.0\n")
-    file.write("0.8978   4.71296   12.839416         0.0\n")
-    file.write("0.9182   4.70303   12.701343         0.0\n")
-    file.write("0.9387   4.60921   12.317719         0.0\n")
-    file.write("0.9591   4.34937   11.470356         0.0\n")
-    file.write("0.9795   3.77288   9.7460480         0.0\n")
+
+    for r, ctrs, cprs in zip(radial_stations, radial_thrust_coefs, radial_power_coefs):
+        file.write(f"{r:.5f}     {ctrs:.5f}      {cprs:08.5f}     0.0\n")
+
     file.write("\n")
 
     return file
---
+++ ceasiompy/SU2Run/tests/tests_su2actuatordisk/test_su2actuatordisk.py
@@ -0,0 +1,71 @@
+"""
+CEASIOMpy: Conceptual Aircraft Design Software
+
+Developed by CFS ENGINEERING, 1015 Lausanne, Switzerland
+
+Test functions of 'ceasiompy/SU2Run/func/su2actuatordisk.py'
+
+Python version: >=3.8
+
+
+| Author : Aidan Jungo and Giacomo Benedetti
+| Creation: 2022-12-05
+
+"""
+
+# =================================================================================================
+#   IMPORTS
+# =================================================================================================
+
+from pathlib import Path
+import numpy as np
+import pytest
+
+from ceasiompy.SU2Run.func.su2actuatordiskfile import write_actuator_disk_data
+
+MODULE_DIR = Path(__file__).parent
+
+# =================================================================================================
+#   CLASSES
+# =================================================================================================
+
+
+# =================================================================================================
+#   FUNCTIONS
+# =================================================================================================
+
+
+def test_write_actuator_disk_data(tmp_path):
+    """Test function 'write_actuator_disk_data'"""
+
+    correct_actuatordisck_path = Path(MODULE_DIR, "correct_ActuatorDisk.dat")
+    test_actuatordisk_path = Path(tmp_path, "ActuatorDisk.dat")
+    
+    with open(test_actuatordisk_path, "w") as f:
+
+        write_actuator_disk_data(
+            file=f,
+            inlet_marker="Inlet",
+            outlet_marker="Outlet",
+            center=(0, 5, 0),
+            axis=(1, 0, 0),
+            radius=2.5,
+            advance_ratio=1.5,
+            radial_stations=np.array([0.2, 0.4, 0.6, 0.8, 1.0]),
+            radial_thrust_coefs=np.array([0.02, 0.04, 0.06, 0.08, 0.06]),
+            radial_power_coefs=np.array([0.1, 0.2, 0.3, 0.4, 0.3]),
+        )
+
+    assert test_actuatordisk_path.read_text() == correct_actuatordisck_path.read_text()
+
+
+
+# =================================================================================================
+#    MAIN
+# =================================================================================================
+
+if __name__ == "__main__":
+
+    print("Test configfile.py")
+    print("To run test use the following command:")
+    print(">> pytest -v")

110860f4345efdb23baedbea77bfcf88068ad70f
GBenedett <giacomo.benedetti@cfse.ch>
2023-10-03 11:28:46 +0200

    sumo profile detection and LE, TE mesh improved

--- ceasiompy/CPACS2SUMO/cpacs2sumo.py
+++ ceasiompy/CPACS2SUMO/cpacs2sumo.py
@@ -627,25 +627,25 @@ def convert_cpacs_to_sumo(cpacs_path, cpacs_out_path):
                 # then TE(1 0), but not reverse way.
 
                 # to avoid double zero, not accepted by SUMO
-                for i, item in enumerate(prof_vect_x):
+                #for i, item in enumerate(prof_vect_x):
                     # if not (prof_vect_x[i] == prof_vect_x[i-1] or \
                     #         round(prof_vect_z[i],4) == round(prof_vect_z[i-1],4)):
-                    if round(prof_vect_z[i], 4) != round(prof_vect_z[i - 1], 4):
+                #    if round(prof_vect_z[i], 4) != round(prof_vect_z[i - 1], 4):
+                #        prof_str += (
+                #            str(round(prof_vect_x[i], 4))
+                #            + " "
+                #            + str(round(prof_vect_z[i], 4))
+                #            + " "
+                #        )
+
+                for i in range(0, len(prof_vect_x)):
+                    if ((prof_vect_x[i] - prof_vect_x[i - 1]) ** 2 + (prof_vect_z[i] - prof_vect_z[i - 1]) ** 2) > 1e-8:
                         prof_str += (
                             str(round(prof_vect_x[i], 4))
                             + " "
                             + str(round(prof_vect_z[i], 4))
                             + " "
                         )
-
-                # for i in range(1, len(prof_vect_x)):
-                    # if ((prof_vect_x[i] - prof_vect_x[i - 1]) ** 2 + (prof_vect_z[i] - prof_vect_z[i - 1]) ** 2) < 1e-6:
-                        # prof_str += (
-                            # str(round(prof_vect_x[i], 4))
-                            # + " "
-                            # + str(round(prof_vect_z[i], 4))
-                            # + " "
-                        # )
 
 
                 sumo.addTextElementAtIndex(wg_sk_xpath, "WingSection", prof_str, wing_sec_index)

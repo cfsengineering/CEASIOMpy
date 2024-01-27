e4a1314de6d0acc2e5d0ce1e14bcabf50565321b
Mengmeng Zhang <mengmeng.zha@gmail.com>
2024-01-27 15:47:47 +0100

    modified:   edgerun.py clean edgerun

--- ceasiompy/EdgeRun/edgerun.py
+++ ceasiompy/EdgeRun/edgerun.py
@@ -55,70 +55,6 @@ MODULE_NAME = MODULE_DIR.name
 # =================================================================================================
 input_que_script_path = get_edge_queScript_template()
 
-
-def run_edge_multi(wkdir, input_que_script_path, nb_proc=2):
-    """Function to run a multiple Edge calculation.
-
-    Function 'run_edge_multi' will run in the given working directory Edge calculations.
-    The working directory must have a folder structure created by 'SU2Config'/ 'EdgeConfig' module.
-
-    Args:
-        wkdir (Path): Path to the working directory
-        nb_proc (int): Number of processor that should be used to run the calculation in parallel
-    """
-    cpacs = CPACS(cpacs_path)
-
-    edge_mesh = Path(get_value(cpacs.tixi, EDGE_MESH_XPATH))
-
-    if not wkdir.exists():
-        raise OSError(f"The working directory : {wkdir} does not exist!")
-
-    case_dir_name = (
-        f"Case{str(case_nb).zfill(2)}_alt{alt}_mach{round(mach, 2)}"
-        f"_aoa{round(aoa, 1)}_aos{round(aos, 1)}"
-    )
-
-    case_dir_path = Path(wkdir, case_dir_name)
-    if not case_dir_path.exists():
-        case_dir_path.mkdir()
-    output_path = Path(case_dir_path, AINP_CFD_NAME)
-
-    case_dir_list = [dir for dir in wkdir.iterdir() if "Case" in dir.name]
-    if not case_dir_list:
-        raise OSError(f"No Case directory has been found in the working directory: {wkdir}")
-
-    for config_dir in sorted(case_dir_list):
-        current_dir = Path(case_dir_path, config_dir)
-
-        config_cfd = [c for c in config_dir.iterdir() if c.name == AINP_CFD_NAME]
-
-        if not config_cfd:
-            raise ValueError(f"No '{AINP_CFD_NAME}' file has been found in this directory!")
-
-        if len(config_cfd) > 1:
-            raise ValueError(f"More than one '{AINP_CFD_NAME}' file in this directory!")
-
-        # run / submit edge commands
-        edge_scripts_instance = EdgeScripts(current_dir, input_que_script_path, AINP_CFD_NAME)
-        # check if preprocessor is already run
-        bedg_files_exist = True
-        for i in range(1, nb_proc + 1):
-            bedg_file_path = Path(case_dir_path, grid_folder, f"Edge.bedg_p{i}")
-            if not bedg_file_path.exists():
-                bedg_files_exist = False
-                break
-        if not bedg_files_exist:
-            # edge_scripts_instance.submit_preprocessor_script(case_dir_path)
-            edge_scripts_instance.run_preprocessor(case_dir_path)
-            print("bedg files are generated")
-
-        # edge_scripts_instance.submit_solver_script(nb_proc)
-        edge_scripts_instance.run_solver(nb_proc)
-
-        # postprocess for results
-        edge_scripts_instance.postprocess_script(case_dir_path, edge_mesh)
-
-
 def extract_edge_forces(results_dir):
     # Use list comprehension to get a list of directory names starting with "Case"
     dir_names = [
@@ -185,8 +121,7 @@ def extract_edge_forces(results_dir):
 def main(cpacs_path, cpacs_out_path):
     log.info("----- Start of " + MODULE_NAME + " -----")
 
-    # tixi = open_tixi(cpacs_path)
-    # nb_proc = get_value_or_default(tixi, EDGE_NB_CPU_XPATH, get_reasonable_nb_cpu())
+    cpacs = CPACS(cpacs_path)
 
     results_dir = get_results_directory("EdgeRun")
 
@@ -198,9 +133,7 @@ def main(cpacs_path, cpacs_out_path):
     log.info("Edge postprocessing started")
     extract_edge_forces(results_dir)
     log.info("Edge postprocessing finished")
-    # run_edge_multi(results_dir, nb_proc)
-    # get_su2_results(cpacs_tmp_cfg, cpacs_out_path, results_dir)
-    cpacs = CPACS(cpacs_path)
+
     cpacs.save_cpacs(cpacs_out_path, overwrite=True)
     
     log.info("----- End of " + MODULE_NAME + " -----")

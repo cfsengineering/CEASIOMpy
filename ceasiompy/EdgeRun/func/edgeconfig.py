a9c837c8a613716d8ee2c11ed562448562aab1be
Mengmeng Zhang <mengmeng.zha@gmail.com>
2024-01-27 15:49:10 +0100

    modified:   func/edgeconfig.py added postprocessor

--- ceasiompy/EdgeRun/func/edgeconfig.py
+++ ceasiompy/EdgeRun/func/edgeconfig.py
@@ -320,7 +320,9 @@ def edge_cfd(cpacs_path, cpacs_out_path, wkdir):
         log.info("Edge solver is done." )
 
         # postprocess for results
-        #edge_scripts_instance.postprocess_script(case_dir_path, edge_mesh)
+        log.info("Running Edge postprocessor...for " + case_dir_name )
+        edge_scripts_instance.postprocess_script(case_dir_path, edge_mesh)
+        log.info("Edge postprocessor is done." )
         # wait until the results are generated
         
         cpacs.save_cpacs(cpacs_out_path, overwrite=True)

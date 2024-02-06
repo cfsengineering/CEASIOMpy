6270c5950a753eb765036767bc978356cbddc9c0
GBenedett <giacomo.benedetti@cfse.ch>
2024-02-06 13:59:04 +0100

    Remove space for markers created by pentagrow

--- ceasiompy/SU2Run/func/su2config.py
+++ ceasiompy/SU2Run/func/su2config.py
@@ -317,6 +317,11 @@ def generate_su2_cfd_config(cpacs_path, cpacs_out_path, wkdir):
         raise FileNotFoundError(f"SU2 mesh file {su2_mesh} not found")
 
     mesh_markers = get_mesh_markers(su2_mesh)
+    for key, value in mesh_markers.items():
+        # Verifica se value è una lista
+        if isinstance(value, list):
+            # Applica replace a ciascun elemento della lista
+            mesh_markers[key] = [str(item).replace(" ", "") for item in value]
 
     create_branch(cpacs.tixi, SU2_BC_WALL_XPATH)
     bc_wall_str = ";".join(mesh_markers["wall"])
@@ -341,7 +346,7 @@ def generate_su2_cfd_config(cpacs_path, cpacs_out_path, wkdir):
 
         if aeromap_list:
             aeromap_default = aeromap_list[0]
-            log.info(f'The aeromap is {aeromap_default}')
+            log.info(f"The aeromap is {aeromap_default}")
 
             aeromap_uid = get_value_or_default(cpacs.tixi, SU2_AEROMAP_UID_XPATH, aeromap_default)
 

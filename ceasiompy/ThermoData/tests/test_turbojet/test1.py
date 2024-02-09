c22ed030b1cb98cbc37d76d17eea18f6a119eef6
Francesco-Marcucci <fra.marcuc@libero.it>
2024-02-09 10:35:04 +0100

    start adding tests

---
+++ ceasiompy/ThermoData/tests/test_turbojet/test1.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+import sys
+
+sys.path.append("/home/cfse/Stage_Francesco/Thermodata")
+
+from ceasiompy.ThermoData.func.turbofan_func import (
+    turbofan_analysis,
+)
+
+from ceasiompy.ThermoData.func.turbojet_func import (
+    turbojet_analysis,
+)
+
+import pytest
+
+import numpy as np
+
+
+def test_turbojet():
+    alt = 4000  # [ft]
+    MN = 0.6
+    Fn = 11800  # [lbf]
+    new_sol = turbojet_analysis(alt, MN, Fn)
+    correct_sol = np.array([717.59577021, 818.40663844, 1.57394908]).reshape((3, 1))
+    np.testing.assert_almost_equal(new_sol, correct_sol, 5)
+
+
+def test_turbofan():
+    alt = 4000  # [ft]
+    MN = 0.6
+    Fn = 11800  # [lbf]
+    new_sol_tf = turbofan_analysis(alt, MN)
+    correct_sol_tf = np.array(
+        [
+            8.11328662e01,
+            3.44500820e02,
+            1.00000000e00,
+            4.19439630e02,
+            5.64233274e01,
+            1.08865912e-01,
+        ]
+    ).reshape((6, 1))
+    np.testing.assert_almost_equal(new_sol_tf, correct_sol_tf, 5)
---
+++ ceasiompy/ThermoData/tests/test_turbojet/test_turbojet.py
@@ -0,0 +1,59 @@
+"""
+CEASIOMpy: Conceptual Aircraft Design Software
+
+Developed by CFS ENGINEERING, 1015 Lausanne, Switzerland
+
+Test functions of 'ceasiompy/ThermoData/func/turbojet_func.py'
+
+Python version: >=3.8
+
+
+| Author : Giacomo Benedetti and Francesco Marcucci
+| Creation: 2024-02-09
+
+"""
+# =================================================================================================
+#   IMPORTS
+# =================================================================================================
+
+import os
+from pathlib import Path
+
+import pytest
+from ceasiompy.SU2Run.func.su2results import save_screenshot
+from ceasiompy.utils.commonpaths import TEST_RESULTS_FILES_PATH
+
+MODULE_DIR = Path(__file__).parent
+
+
+# =================================================================================================
+#   CLASSES
+# =================================================================================================
+
+
+# =================================================================================================
+#   FUNCTIONS
+# =================================================================================================
+
+
+@pytest.mark.skipif("DISPLAY" not in os.environ, reason="requires display")
+def test_save_screenshot():
+    """Test the function 'save_screenshot'"""
+
+    surface_flow_file = Path(TEST_RESULTS_FILES_PATH, "surface_flow.vtu")
+    screenshot_file = save_screenshot(surface_flow_file, "Mach")
+    assert screenshot_file.exists()
+
+    if screenshot_file.exists():
+        screenshot_file.unlink()
+
+
+# =================================================================================================
+#    MAIN
+# =================================================================================================
+
+if __name__ == "__main__":
+
+    print("Test configfile.py")
+    print("To run test use the following command:")
+    print(">> pytest -v")

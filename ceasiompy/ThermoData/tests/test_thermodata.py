39f1a3e604542129125d27bbba244c9952756b09
Francesco-Marcucci <fra.marcuc@libero.it>
2024-03-18 15:30:04 +0100

    finishing test implementation

--- ceasiompy/ThermoData/tests/test_thermodata.py
+++ ceasiompy/ThermoData/tests/test_thermodata.py
@@ -27,6 +27,11 @@ from ceasiompy.ThermoData.func.turbojet_func import (
     write_turbojet_file,
 )
 
+from ceasiompy.ThermoData.func.turbofan_func import (
+    turbofan_analysis,
+    write_hbtf_file,
+)
+
 sys.path.append("/home/cfse/Stage_Francesco/Thermodata")
 
 # =================================================================================================
@@ -88,6 +93,78 @@ def test_write_turbojet_file(tmp_path):
     # print("content=", content)
     # expected_content = test_thermodata_path.read_text().split("\n")
     # print("expected_content=", expected_content)
+
+    assert test_thermodata_path.read_text().split("\n") == content
+
+
+def test_turbofan_func():
+    alt = 1000
+    MN = 0.3
+    Fn = 2000
+    new_sol_tuple = turbofan_analysis(alt, MN, Fn)
+    new_sol = np.concatenate(new_sol_tuple)
+    correct_sol = np.array(
+        [
+            65.04136793,
+            323.23292298,
+            0.95295201,
+            161198.12289882,
+            1.78761453,
+            13.08931717,
+            1270.61685498,
+            724.51083329,
+            1.0,
+            964349.32127573,
+            1.42155524,
+            1139.22870449,
+        ]
+    )
+    print(new_sol)
+    print("hello")
+    print(correct_sol)
+    np.testing.assert_almost_equal(new_sol, correct_sol, 3)
+
+
+def test_write_hbtf_file(tmp_path):
+    T_tot_out_core = 300
+    V_stat_out_core = 200
+    MN_out_core = 0.5
+    P_tot_out_core = 100000
+    massflow_out_core = 50
+    T_stat_out_core = 400
+    T_tot_out_byp = 200000
+    V_stat_out_byp = 300
+    MN_out_byp = 1.2
+    P_tot_out_byp = 100000
+    massflow_stat_out_byp = 1.6
+    T_stat_out_byp = 13
+
+    test_thermodata_path = Path(tmp_path, "EngineBC.dat")
+
+    with open(test_thermodata_path, "w") as file:
+        write_hbtf_file(
+            file,
+            T_tot_out_core,
+            V_stat_out_core,
+            MN_out_core,
+            P_tot_out_core,
+            massflow_out_core,
+            T_stat_out_core,
+            T_tot_out_byp,
+            V_stat_out_byp,
+            MN_out_byp,
+            P_tot_out_byp,
+            massflow_stat_out_byp,
+            T_stat_out_byp,
+        )
+
+    with open(test_thermodata_path, "r") as file:
+        content = [line.strip() for line in file.readlines()]
+    content.append("")
+
+    print("content=", content)
+    expected_content = test_thermodata_path.read_text().split("\n")
+    print("expected_content=", expected_content)
 
     assert test_thermodata_path.read_text().split("\n") == content
 

6f285a82441a3128f3e6d7a04944b8ef1febaaeb
Romain-Gauthier <romain.gauthier@epfl.ch>
2024-06-12 14:35:54 +0200

    print case dir path

--- ceasiompy/PyAVL/avlrun.py
+++ ceasiompy/PyAVL/avlrun.py
@@ -96,7 +96,9 @@ def run_avl(cpacs_path, wkdir):
             ref_density=density,
             g_acceleration=g,
         )
-        subprocess.run(["avl"], stdin=open(str(command_path), "r"), cwd=case_dir_path)
+
+        print("##############", case_dir_path)
+        subprocess.run(["xvfb-run", "avl"], stdin=open(str(command_path), "r"), cwd=case_dir_path)
 
         if save_fig:
             subprocess.run(["ps2pdf", "plot.ps", "plot.pdf"], cwd=case_dir_path)

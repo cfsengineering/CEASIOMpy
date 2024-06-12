3ba8739f87f6e8d914ad89843adfa516ed8c1bc4
GBenedett <giacomo.benedetti@cfse.ch>
2024-06-12 11:11:17 +0200

    skip integration test for avl

--- ceasiompy/PyAVL/tests/test_avlresults.py
+++ ceasiompy/PyAVL/tests/test_avlresults.py
@@ -17,15 +17,14 @@ Python version: >=3.8
 # =================================================================================================
 from pathlib import Path
 import pytest
-from ceasiompy.PyAVL.func.avlresults import (
-    get_avl_aerocoefs,
-    plot_lift_distribution)
+from ceasiompy.PyAVL.func.avlresults import get_avl_aerocoefs, plot_lift_distribution
 
 from ceasiompy.utils.commonpaths import CPACS_FILES_PATH
 
 MODULE_DIR = Path(__file__).parent
 
 CPACS_IN_PATH = Path(CPACS_FILES_PATH, "labARscaled.xml")
+FT_TEMPLATE = Path(MODULE_DIR, "ft_template.txt")
 
 # =================================================================================================
 #   CLASSES
@@ -36,23 +35,23 @@ CPACS_IN_PATH = Path(CPACS_FILES_PATH, "labARscaled.xml")
 #   FUNCTIONS
 # =================================================================================================
 
-def test_plot_lift_distribution(tmp_path):
 
+def test_plot_lift_distribution(tmp_path):
     plot_lift_distribution(
         force_file_fs=Path(MODULE_DIR, "fs_template.txt"),
         aoa=5,
         aos=0,
         mach=0.3,
         alt=1000,
-        wkdir=tmp_path)
+        wkdir=tmp_path,
+    )
 
-    assert Path(tmp_path, "lift_distribution.png").exists(
-    ), "Plot 'lift_distribution.png' does not exist."
+    assert Path(
+        tmp_path, "lift_distribution.png"
+    ).exists(), "Plot 'lift_distribution.png' does not exist."
 
 
 def test_get_avl_aerocoefs():
-    FT_TEMPLATE = Path(MODULE_DIR, "ft_template.txt")
-
     assert FT_TEMPLATE.exists(), "Result file 'ft.txt' not found!"
     cl, cd, cm = get_avl_aerocoefs(FT_TEMPLATE)
     assert cl == pytest.approx(0.35063, rel=1e-4), "CLtot is not correct!"

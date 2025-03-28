a6cddfa2039a7d87cc6cecfeff7d4dae8d3c2249
GuidoValli97 <guidovalli@hotm>
2025-03-28 08:51:06 +0100

    Un-used variable

--- ceasiompy/DynamicStability/func/steadyderivatives.py
+++ ceasiompy/DynamicStability/func/steadyderivatives.py
@@ -30,6 +30,7 @@ from ceasiompy.PyAVL import SOFTWARE_NAME as AVL_SOFTWARE
 #   FUNCTIONS
 # =================================================================================================
 
+
 def format_aero_data(df: DataFrame) -> DataFrame:
     """
     Format the data into the specified sequence.
@@ -38,37 +39,38 @@ def format_aero_data(df: DataFrame) -> DataFrame:
 
     # Group by Mach number
     mach_groups = df.groupby("mach")
-    for mach, mach_group in mach_groups:
+    for _, mach_group in mach_groups:
         # Sort by alpha
         mach_group = mach_group.sort_values(by="alpha")
 
         # Extract the 9 series
-        formatted_data.append(mach_group[(mach_group["beta"] == 0) & 
-                                         (mach_group["pb_2V"] == 0) & 
-                                         (mach_group["qc_2V"] == 0) & 
-                                         (mach_group["rb_2V"] == 0)])
-        
+        formatted_data.append(mach_group[(mach_group["beta"] == 0)
+                                         & (mach_group["pb_2V"] == 0)
+                                         & (mach_group["qc_2V"] == 0)
+                                         & (mach_group["rb_2V"] == 0)])
+
         # Sort by beta with q=p=r=0
-        formatted_data.append(mach_group[(mach_group["pb_2V"] == 0) & 
-                                         (mach_group["qc_2V"] == 0) & 
-                                         (mach_group["rb_2V"] == 0)].sort_values(by="beta"))
-        
+        formatted_data.append(mach_group[(mach_group["pb_2V"] == 0)
+                                         & (mach_group["qc_2V"] == 0)
+                                         & (mach_group["rb_2V"] == 0)].sort_values(by="beta"))
+
         # Sort by q with beta=p=r=0
-        formatted_data.append(mach_group[(mach_group["beta"] == 0) & 
-                                         (mach_group["pb_2V"] == 0) & 
-                                         (mach_group["rb_2V"] == 0)].sort_values(by="qc_2V"))
-        
+        formatted_data.append(mach_group[(mach_group["beta"] == 0)
+                                         & (mach_group["pb_2V"] == 0)
+                                         & (mach_group["rb_2V"] == 0)].sort_values(by="qc_2V"))
+
         # Sort by p with beta=q=r=0
-        formatted_data.append(mach_group[(mach_group["beta"] == 0) & 
-                                         (mach_group["qc_2V"] == 0) & 
-                                         (mach_group["rb_2V"] == 0)].sort_values(by="pb_2V"))
-        
+        formatted_data.append(mach_group[(mach_group["beta"] == 0)
+                                         & (mach_group["qc_2V"] == 0)
+                                         & (mach_group["rb_2V"] == 0)].sort_values(by="pb_2V"))
+
         # Sort by r with beta=q=p=0
-        formatted_data.append(mach_group[(mach_group["beta"] == 0) & 
-                                         (mach_group["pb_2V"] == 0) & 
-                                         (mach_group["qc_2V"] == 0)].sort_values(by="rb_2V"))
+        formatted_data.append(mach_group[(mach_group["beta"] == 0)
+                                         & (mach_group["pb_2V"] == 0)
+                                         & (mach_group["qc_2V"] == 0)].sort_values(by="rb_2V"))
 
     return concat(formatted_data, ignore_index=True)
+
 
 def format_ctrl_data(df: DataFrame) -> DataFrame:
     """
@@ -83,23 +85,24 @@ def format_ctrl_data(df: DataFrame) -> DataFrame:
         mach_group = mach_group.sort_values(by="alpha")
 
         # Extract the 9 series
-        formatted_data.append(mach_group[(mach_group["elevator"] == 0) & 
-                                         (mach_group["rudder"] == 0) & 
-                                         (mach_group["aileron"] == 0)])
-        
+        formatted_data.append(mach_group[(mach_group["elevator"] == 0)
+                                         & (mach_group["rudder"] == 0)
+                                         & (mach_group["aileron"] == 0)])
+
         # Sort by elevator with rudder=aileron=0
-        formatted_data.append(mach_group[(mach_group["rudder"] == 0) & 
-                                         (mach_group["aileron"] == 0)].sort_values(by="elevator"))
-        
+        formatted_data.append(mach_group[(mach_group["rudder"] == 0)
+                                         & (mach_group["aileron"] == 0)].sort_values(by="elevator"))
+
         # Sort by rudder with elevator=aileron=0
-        formatted_data.append(mach_group[(mach_group["elevator"] == 0) & 
-                                         (mach_group["aileron"] == 0)].sort_values(by="rudder"))
-        
+        formatted_data.append(mach_group[(mach_group["elevator"] == 0)
+                                         & (mach_group["aileron"] == 0)].sort_values(by="rudder"))
+
         # Sort by aileron with elevator=rudder=0
-        formatted_data.append(mach_group[(mach_group["elevator"] == 0) & 
-                                         (mach_group["rudder"] == 0)].sort_values(by="aileron"))
+        formatted_data.append(mach_group[(mach_group["elevator"] == 0)
+                                         & (mach_group["rudder"] == 0)].sort_values(by="aileron"))
 
     return concat(formatted_data, ignore_index=True)
+
 
 def get_tables_values(
     self
@@ -119,17 +122,17 @@ def get_tables_values(
         table_name = "avl_data"
     else:
         log.warning(f"software {self.software_data} not implemented yet.")
-        
+
     aero_columns = [
-        "alpha", "mach", "beta", "pb_2V", "qc_2V", "rb_2V", 
-        "cl", "cd", "cms" ,"cs", "cmd", "cml"
+        "alpha", "mach", "beta", "pb_2V", "qc_2V", "rb_2V",
+        "cl", "cd", "cms" , "cs", "cmd", "cml"
     ]
 
     # Retrieve data from db
     ceasiompy_db = CeasiompyDb()
     data = ceasiompy_db.get_data(
-        table_name=table_name, 
-        columns=aero_columns, 
+        table_name=table_name,
+        columns=aero_columns,
         filters=[
             f"mach IN ({self.mach_str})",
             f"aircraft = '{self.aircraft_name}'",
@@ -139,15 +142,15 @@ def get_tables_values(
 
     # Convert the data list to a DataFrame
     aero_df = DataFrame(data, columns=aero_columns)
-    
+
     ctrl_columns = [
-        "alpha", "mach", "elevator", "rudder", "aileron", 
-        "cl", "cd", "cms" ,"cs", "cmd", "cml"
+        "alpha", "mach", "elevator", "rudder", "aileron",
+        "cl", "cd", "cms" , "cs", "cmd", "cml"
     ]
-    
+
     data = ceasiompy_db.get_data(
-        table_name=table_name, 
-        columns=ctrl_columns, 
+        table_name=table_name,
+        columns=ctrl_columns,
         filters=[
             f"mach IN ({self.mach_str})",
             f"aircraft = '{self.aircraft_name}'",

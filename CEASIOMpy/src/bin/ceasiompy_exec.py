cfa0743c989ae4190ae54ae986c835b19c3ec9b5
LeonDeligny <leon.deligny@icloud.com>
2025-03-31 10:10:27 +0200

    Update submodule reference

---
+++ CEASIOMpy/src/bin/ceasiompy_exec.py
@@ -0,0 +1,240 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+CEASIOMpy: Conceptual Aircraft Design Software
+
+Developed by CFS ENGINEERING, 1015 Lausanne, Switzerland
+
+Main module of CEASIOMpy to launch workflow by different way.
+
+Python version: >=3.8
+
+| Author: Aidan Jungo
+| Creation: 2022-03-29
+
+"""
+
+# =================================================================================================
+#   IMPORTS
+# =================================================================================================
+
+import os
+import argparse
+
+from pathlib import Path
+from ceasiompy.utils.workflowclasses import Workflow
+
+from ceasiompy import log
+
+from ceasiompy.utils.commonpaths import (
+    STREAMLIT_PATH,
+    TEST_CASES_PATH,
+    CPACS_FILES_PATH,
+)
+
+# =================================================================================================
+#   FUNCTIONS
+# =================================================================================================
+
+
+def testcase_message(testcase_nb):
+    """Top message to show when a test case is run."""
+
+    log.info(f"CEASIOMpy as been started from test case {testcase_nb}")
+
+    print("\n")
+    print("#" * 30)
+    print(f"### CEASIOMpy: Test case {testcase_nb} ###")
+    print("#" * 30)
+    print("More information about this test case at:")
+    print(
+        "https://github.com/cfsengineering/CEASIOMpy/blob/main/"
+        f"test_cases/test_case_{testcase_nb}/README.md"
+    )
+    print("\n")
+
+
+def run_testcase(testcase_nb):
+    """Run a test case."""
+
+    if testcase_nb == 1:
+
+        testcase_message(1)
+
+        test_case_1_path = Path(TEST_CASES_PATH, "test_case_1")
+        test_case_1_cfg = Path(test_case_1_path, "testcase1.cfg")
+
+        workflow = Workflow()
+        workflow.from_config_file(test_case_1_cfg)
+        workflow.working_dir = Path().cwd()
+        workflow.cpacs_in = Path(CPACS_FILES_PATH, "D150_simple.xml")
+
+        workflow.set_workflow()
+        workflow.run_workflow()
+
+        print("\nCongratulation, this Test case is now finished!")
+        print(f"\nYou can check your results in: {workflow.current_wkflow_dir}/Results")
+
+    elif testcase_nb == 2:
+        testcase_message(2)
+        print("\nUse the GUI to create your workflow.")
+        run_gui()
+        print("\nCongratulation, this Test case is now finished!")
+
+    elif testcase_nb == 3:
+        testcase_message(3)
+        print("\nUse the GUI to create your workflow.")
+        run_gui()
+        print("\nCongratulation, this Test case is now finished!")
+
+    elif testcase_nb == 4:
+        testcase_message(4)
+        print(
+            "To run this test case, you will need to open a terminal "
+            "and run the following command:"
+        )
+        print(">> conda activate ceasiompy")
+        print(">> cd WKDIR")
+        print(
+            ">> ceasiompy_run -m ../test_files/CPACSfiles/D150_simple.xml "
+            "PyTornado SkinFriction SaveAeroCoefficients"
+        )
+
+    elif testcase_nb == 5:
+        testcase_message(5)
+
+    else:
+        print("\nTest case number must be 1,2,3,4 or 5.")
+
+
+def run_modules_list(args_list, test=False):
+    """Run a workflow from a CPACS file and a list of modules."""
+
+    if len(args_list) < 2:
+        print(
+            "\nAt least 2 arguments are required to run a CEASIOMpy, the first onw must be the"
+            "CPACS file and the modules to run. You can add as many modules as you want."
+        )
+        return
+
+    cpacs_path = Path(args_list[0])
+
+    if cpacs_path.suffix != ".xml":
+        print(
+            'The first argument of "-m/--modules" option must be the path to a CPACS file.',
+        )
+        return
+
+    if not cpacs_path.exists():
+        print(f"The CPACS file {cpacs_path} does not exist.")
+        return
+
+    modules_list = args_list[1:]
+
+    log.info("CEASIOMpy as been started from a command line")
+
+    workflow = Workflow()
+    workflow.cpacs_in = cpacs_path
+    workflow.modules_list = modules_list
+    workflow.module_optim = ["NO"] * len(modules_list)
+    workflow.write_config_file()
+
+    workflow.set_workflow()
+    workflow.run_workflow(test)
+
+
+def run_config_file(config_file):
+    """Run a workflow from a config file"""
+
+    log.info("CEASIOMpy has been started from a config file.")
+
+    config_file_path = Path(config_file)
+
+    if not config_file_path.exists():
+        print(f"The config file {config_file_path} does not exist.")
+        return
+
+    workflow = Workflow()
+    workflow.from_config_file(config_file_path)
+
+    workflow.set_workflow()
+    workflow.run_workflow()
+
+
+def run_gui():
+    """Create an run a workflow from a GUI."""
+
+    log.info("CEASIOMpy has been started from the GUI.")
+    os.system(f"cd {STREAMLIT_PATH} && streamlit run CEASIOMpy.py")
+
+# =================================================================================================
+#    MAIN
+# =================================================================================================
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="CEASIOMpy: Conceptual Aircraft Design Environment",
+        usage=argparse.SUPPRESS,
+        formatter_class=lambda prog: argparse.HelpFormatter(prog, max_help_position=80, width=99),
+    )
+
+    parser.add_argument(
+        "-c",
+        "--cfg",
+        type=str,
+        metavar="PATH",
+        help="create a CEASIOMpy workflow from a configuration file [PATH to the config file]",
+    )
+    parser.add_argument(
+        "-g",
+        "--gui",
+        action="store_true",
+        help="create a CEASIOMpy workflow with the Graphical user interface",
+    )
+    parser.add_argument(
+        "-m",
+        "--modules",
+        nargs="+",
+        metavar="",
+        default=[],
+        help="create a CEASIOMpy workflow by giving the CPACS file and the list of module to run"
+        " [Path to the CPACS] [Module list]",
+    )
+    parser.add_argument(
+        "--testcase",
+        type=int,
+        metavar="NB",
+        help="run a test case [1, 2, or 3]",
+    )
+
+    args = parser.parse_args()
+
+    if args.testcase:
+
+        run_testcase(args.testcase)
+        return
+
+    if args.modules:
+
+        run_modules_list(args.modules)
+        return
+
+    if args.cfg:
+
+        run_config_file(args.cfg)
+
+        return
+
+    if args.gui:
+        run_gui()
+        return
+
+    # If no argument is given, print the help
+    parser.print_help()
+
+
+if __name__ == "__main__":
+
+    main()
---
+++ CEASIOMpy/src/streamlit/CEASIOMpy.py
@@ -0,0 +1,162 @@
+"""
+CEASIOMpy: Conceptual Aircraft Design Software
+
+Developed for CFS ENGINEERING, 1015 Lausanne, Switzerland
+
+Main Streamlit page for CEASIOMpy GUI.
+
+Python version: >=3.8
+
+| Author : Aidan Jungo
+| Creation: 2022-09-09
+
+"""
+
+# =================================================================================================
+#    IMPORTS
+# =================================================================================================
+
+
+import io
+
+import streamlit as st
+import pyvista as pv
+import streamlit.components.v1 as components
+
+from streamlitutils import create_sidebar, st_directory_picker
+
+from ceasiompy.utils.workflowclasses import Workflow
+from cpacspy.cpacspy import CPACS
+from pathlib import Path
+
+from ceasiompy.utils.commonnames import CEASIOMPY_BEIGE, CEASIOMPY_ORANGE
+
+# =================================================================================================
+#    CONSTANTS
+# =================================================================================================
+
+HOW_TO_TEXT = (
+    "### How to use CEASIOMpy?\n"
+    "1. Choose a *Working directory*\n"
+    "1. Choose a *CPACS file*\n"
+    "1. Go to *Workflow* page (with menu above)\n"
+)
+
+PAGE_NAME = "CEASIOMpy"
+
+# =================================================================================================
+#    FUNCTIONS
+# =================================================================================================
+
+
+def section_select_working_dir():
+
+    st.markdown("#### Working directory")
+
+    if "workflow" not in st.session_state:
+        st.session_state.workflow = Workflow()
+
+    st.session_state.workflow.working_dir = st_directory_picker(Path("../../WKDIR").absolute())
+
+
+def section_select_cpacs():
+    st.markdown("#### CPACS file")
+
+    # Check if the CPACS file path is already in session state
+    if "cpacs_file_path" in st.session_state:
+        cpacs_file_path = st.session_state.cpacs_file_path
+        if Path(cpacs_file_path).exists():
+            st.session_state.cpacs = CPACS(cpacs_file_path)
+            # st.info(f"**Aircraft name:** {st.session_state.cpacs.ac_name}")
+        else:
+            st.session_state.cpacs_file_path = None
+
+    # File uploader widget
+    uploaded_file = st.file_uploader("Select a CPACS file", type=["xml"])
+
+    if uploaded_file:
+        cpacs_new_path = Path(st.session_state.workflow.working_dir, uploaded_file.name)
+
+        with open(cpacs_new_path, "wb") as f:
+            f.write(uploaded_file.getbuffer())
+
+        st.session_state.workflow.cpacs_in = cpacs_new_path
+        st.session_state.cpacs = CPACS(cpacs_new_path)
+        st.session_state.cpacs_file_path = str(cpacs_new_path)
+
+        # st.info(f"**Aircraft name:** {st.session_state.cpacs.ac_name}")
+
+    # Display the file uploader widget with the previously uploaded file
+    if "cpacs_file_path" in st.session_state and st.session_state.cpacs_file_path:
+        st.write(f"Uploaded file: {st.session_state.cpacs_file_path}")
+
+
+def section_3D_view():
+    """
+    Shows a 3D view of the aircraft by exporting a STL file. The pyvista viewer is based on:
+    https://github.com/edsaac/streamlit-PyVista-viewer
+    """
+
+    st.markdown("## 3D view")
+
+    if "cpacs" not in st.session_state:
+        st.warning("No CPACS file has been selected!")
+        return
+
+    if not st.button("Show 3D view"):
+        return
+
+    stl_file = Path(st.session_state.workflow.working_dir, "aircraft.stl")
+
+    st.session_state.cpacs.aircraft.tigl.exportMeshedGeometrySTL(str(stl_file), 0.01)
+
+    # Using pythreejs as pyvista backend
+    pv.set_jupyter_backend("pythreejs")
+
+    # Initialize pyvista reader and plotter
+    plotter = pv.Plotter(border=False, window_size=[572, 600])
+    plotter.background_color = CEASIOMPY_BEIGE
+    reader = pv.STLReader(str(stl_file))
+
+    # Read data and send to plotter
+    mesh = reader.read()
+    plotter.add_mesh(mesh, color=CEASIOMPY_ORANGE)
+
+    # Camera
+    plotter.camera.azimuth = 110.0
+    plotter.camera.elevation = -20.0
+    plotter.camera.zoom(1.4)
+
+    # Export to a pythreejs HTML
+    model_html = io.StringIO()
+    plotter.export_html(model_html, backend="pythreejs")
+
+    # Show in webpage
+    components.html(model_html.getvalue(), height=600, width=572, scrolling=False)
+
+# =================================================================================================
+#    MAIN
+# =================================================================================================
+
+
+if __name__ == "__main__":
+
+    create_sidebar(HOW_TO_TEXT)
+    st.markdown("""
+        <style>
+        /* Align navigation buttons with selectbox */
+        .nav-button-container {
+            display: flex;
+            align-items: flex-end;
+            height: 100%;
+            padding-bottom: 5px;
+            margin-top: 23px;  /* Matches the label height + spacing */
+        }
+        </style>
+    """, unsafe_allow_html=True)
+
+    st.title(PAGE_NAME)
+
+    section_select_working_dir()
+    section_select_cpacs()
+    section_3D_view()

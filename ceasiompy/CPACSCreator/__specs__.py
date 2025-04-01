a067f0fabf9e77a08455282438da0d1bb7e53262
GBenedett <113425078+GBenedett@users.noreply.github.com>
2025-04-01 17:40:46 +0200

    back to previous version (#310)

---
+++ ceasiompy/CPACSCreator/__specs__.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from ceasiompy.utils.moduleinterfaces import CPACSInOut
+
+# ===== Module Status =====
+# True if the module is active
+# False if the module is disabled (not working or not ready)
+module_status = True
+
+# ===== Results directory path =====
+
+RESULTS_DIR = Path("Results", "CPACSCreator")
+
+# ===== CPACS inputs and outputs =====
+
+cpacs_inout = CPACSInOut()
+
+# ----- Input -----
+
+# No inputs value for this modules
+
+# ----- Output -----
+
+# No outputs value for this modules

76bd0eb30d90a0001f71750205c1f6975651c2c9
LeonDeligny <leon.deligny@icloud.com>
2025-04-01 11:04:08 +0200

    flake8 .

--- CEASIOMpy/ceasiompy/Database/func/__init__.py
+++ CEASIOMpy/ceasiompy/Database/func/__init__.py
@@ -14,84 +14,81 @@ from ceasiompy.DynamicStability import MODULE_NAME as DYNSTAB_NAME
 
 # Tables structure of ceasiompy.db
 TABLE_DICT = {
-    f"{PYAVL_NAME}":
-        [
-            "avl_data",
-            """
-                aircraft TEXT,
+    f"{PYAVL_NAME}": [
+        "avl_data",
+        """
+            aircraft TEXT,
 
-                alt REAL,
-                mach REAL,
-                alpha REAL,
-                beta REAL,
+            alt REAL,
+            mach REAL,
+            alpha REAL,
+            beta REAL,
 
-                pb_2V REAL,
-                qc_2V REAL,
-                rb_2V REAL,
+            pb_2V REAL,
+            qc_2V REAL,
+            rb_2V REAL,
 
-                flap REAL,
-                aileron REAL,
-                elevator REAL,
-                rudder REAL,
+            flap REAL,
+            aileron REAL,
+            elevator REAL,
+            rudder REAL,
 
-                xref REAL,
-                yref REAL,
-                zref REAL,
+            xref REAL,
+            yref REAL,
+            zref REAL,
 
-                cd REAL,
-                cs REAL,
-                cl REAL,
+            cd REAL,
+            cs REAL,
+            cl REAL,
 
-                cmd REAL,
-                cms REAL,
-                cml REAL,
+            cmd REAL,
+            cms REAL,
+            cml REAL,
 
-                cmd_a REAL,
-                cms_a REAL,
-                cml_a REAL,
+            cmd_a REAL,
+            cms_a REAL,
+            cml_a REAL,
 
-                cmd_b REAL,
-                cms_b REAL,
-                cml_b REAL,
-            """
-        ],
-    f"{CPACS2GMSH_NAME}":
-        [
-            "gmsh_data",
-            """
-                aircraft TEXT,
-                deformation TEXT,
-                angle REAL,
-                su2_file_data BLOB,
-            """
-        ],
-    f"{DYNSTAB_NAME}":
-        [
-            "derivatives_data",
-            """
-                aircraft TEXT,
+            cmd_b REAL,
+            cms_b REAL,
+            cml_b REAL,
+        """
+    ],
+    f"{CPACS2GMSH_NAME}": [
+        "gmsh_data",
+        """
+            aircraft TEXT,
+            deformation TEXT,
+            angle REAL,
+            su2_file_data BLOB,
+        """
+    ],
+    f"{DYNSTAB_NAME}": [
+        "derivatives_data",
+        """
+            aircraft TEXT,
 
-                method TEXT,
+            method TEXT,
 
-                chord INT,
-                span INT,
+            chord INT,
+            span INT,
 
-                mach REAL,
+            mach REAL,
 
-                x_ref REAL,
-                y_ref REAL,
-                z_ref REAL,
+            x_ref REAL,
+            y_ref REAL,
+            z_ref REAL,
 
-                cm_alphaprim REAL,
-                cz_alphaprim REAL,
-                cx_alphaprim REAL,
+            cm_alphaprim REAL,
+            cz_alphaprim REAL,
+            cx_alphaprim REAL,
 
-                cy_betaprim REAL,
-                cl_betaprim REAL,
-                cn_betaprim REAL,
-            """
-        ],
-    }
+            cy_betaprim REAL,
+            cl_betaprim REAL,
+            cn_betaprim REAL,
+        """
+    ],
+}
 
 # ==============================================================================
 #    MAIN

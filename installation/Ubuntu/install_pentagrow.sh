7080784ac6d879ad095cf8575458508841ad76a1
GBenedett <giacomo.benedetti@cfse.ch>
2025-04-29 08:54:14 +0200

    echo check

--- installation/Ubuntu/install_pentagrow.sh
+++ installation/Ubuntu/install_pentagrow.sh
@@ -111,9 +111,10 @@ mkdir -p "$pentagrow_run_path"
 
 echo "install_dir: $install_dir"
 echo "Pentagrow run path: $pentagrow_run_path"
+echo "Current directory: $current_dir"
 
 pentagrow_bin_src="$current_dir/../Pentagrow/bin"
-
+echo "Pentagrow binary source path: $pentagrow_bin_src"
 if [ -d "$pentagrow_bin_src" ] && [ -n "$(ls -A "$pentagrow_bin_src" 2>/dev/null)" ]; then
     if cp "$pentagrow_bin_src"/* "$pentagrow_run_path/" 2>/dev/null; then
         echo "Pentagrow executable found and copied successfully."

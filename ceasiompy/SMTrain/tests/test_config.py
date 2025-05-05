6fa928fa0d4ff9117a244873b67a826d9fbf5e0a
LeonDeligny <leon.deligny@icloud.com>
2025-05-05 16:33:35 +0200

    correct config test

--- ceasiompy/SMTrain/tests/test_config.py
+++ ceasiompy/SMTrain/tests/test_config.py
@@ -11,19 +11,12 @@ Test functions for create_data functions in SMTrain module.
 #   IMPORTS
 # =================================================================================================
 
-from copy import deepcopy
 from ceasiompy.utils.decorators import log_test
 from ceasiompy.SMTrain.func.createdata import retrieve_aeromap_data
-from cpacspy.cpacsfunctions import (
-    add_value,
-    create_branch,
-)
 
 from unittest import main
 from pandas import DataFrame
 from ceasiompy.utils.ceasiompytest import CeasiompyTest
-
-from ceasiompy.SMTrain import SMTRAIN_AVL_DATABASE_XPATH
 
 # =================================================================================================
 #   CLASSES
@@ -34,15 +27,8 @@ class TestConfig(CeasiompyTest):
 
     @log_test
     def test_retrieve_aeromap_data(self) -> None:
-        # Load the default test
-        cpacs = deepcopy(self.test_cpacs)
-        tixi = cpacs.tixi
-
-        # Specify to not add data from ceasiompy.db
-        create_branch(tixi, SMTRAIN_AVL_DATABASE_XPATH)
-        add_value(tixi, SMTRAIN_AVL_DATABASE_XPATH, False)
         full_df = retrieve_aeromap_data(
-            cpacs=cpacs,
+            cpacs=self.test_cpacs,
             aeromap_uid="aeromap_empty",
             objective="cl",
         )

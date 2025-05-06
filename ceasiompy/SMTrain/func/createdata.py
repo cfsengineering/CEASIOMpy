1927cb0975a6b0fb1fb7c67d44706db3e5e8d728
Deligny <leon.deligny@icloud.com>
2025-05-06 15:57:25 +0200

    convert max_iters to str format

--- ceasiompy/SMTrain/func/createdata.py
+++ ceasiompy/SMTrain/func/createdata.py
@@ -134,13 +134,13 @@ def launch_su2(
         su2_mesh_path = get_value(tixi, USED_SU2_MESH_XPATH)
 
     su2_mesh_path_type = get_value(tixi, USED_SU2_MESH_XPATH + "type")
-    max_iters = int(get_value(tixi, SU2_MAX_ITER_XPATH))
+    max_iters = str(get_value(tixi, SU2_MAX_ITER_XPATH))
     update_cpacs_from_specs(cpacs, SU2RUN_NAME, test=True)
 
     # Update CPACS with the new aeromap and su2 mesh paths
     tixi.updateTextElement(USED_SU2_MESH_XPATH + "type", su2_mesh_path_type)
     tixi.updateTextElement(SU2_AEROMAP_UID_XPATH, aeromap.uid)
-    tixi.updateIntegerElement(SU2_MAX_ITER_XPATH, max_iters)
+    tixi.updateTextElement(SU2_MAX_ITER_XPATH, max_iters)
 
     if su2mesh is not None:
         tixi.updateTextElement(SU2MESH_XPATH, su2mesh)

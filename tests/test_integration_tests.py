12377c0ce278d92e46c51db03f7e1e533a7feb93
Deligny <leon.deligny@icloud.com>
2025-05-08 15:38:37 +0200

    no integration tests ?

--- tests/test_integration_tests.py
+++ tests/test_integration_tests.py
@@ -4,7 +4,6 @@ CEASIOMpy: Conceptual Aircraft Design Software
 Developed by CFS ENGINEERING, 1015 Lausanne, Switzerland
 
 Integration test for some typical CEASIOMpy workflows.
-
 """
 
 # ====================================================================================================================

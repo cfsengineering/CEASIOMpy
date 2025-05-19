064a756712fe1755c9f9e807b2fdfa7c1c9c227f
LeonDeligny <leon.deligny@icloud.com>
2025-05-19 11:01:33 +0200

    add back mpich versions

--- installation/CentOS/install_su2.sh
+++ installation/CentOS/install_su2.sh
@@ -3,6 +3,7 @@
 # Script to install SU2 on Centos 8
 
 su2_version="8.1.0"
+mpi_version="4.1.1"
 
 current_dir="$(pwd)"
 
@@ -37,7 +38,7 @@ sudo dnf check-update
 sudo dnf upgrade
 
 echo "Installing MPICH..."
-sudo dnf install -y mpich
+sudo dnf install -y mpich=$mpi_version
 
 echo "Adding MPICH path to the .bashrc"
 mpich_path="/usr/bin"

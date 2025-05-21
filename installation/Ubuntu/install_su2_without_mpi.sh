5372e91f9409ce9515bbd9bfb45648ddd534f67f
LeonDeligny <leon.deligny@icloud.com>
2025-05-21 16:29:30 +0200

    with and without mpi su2 installation scripts

--- installation/Ubuntu/install_su2_without_mpi.sh
+++ installation/Ubuntu/install_su2_without_mpi.sh
@@ -17,21 +17,12 @@ echo "Creating install directory..."
 mkdir -p "$install_dir"
 cd "$install_dir"
 
-echo "Installing Open MPI"
-apt-get update && \
-apt-get install -y --no-install-recommends \
-    build-essential \
-    openmpi-bin \
-    openmpi-doc \
-    libopenmpi-dev && \
-rm -rf /var/lib/apt/lists/*
-
 echo "Downloading SU2..."
-wget https://github.com/su2code/SU2/releases/download/v"$su2_version"/SU2-v"$su2_version"-linux64-mpi.zip
-unzip -d SU2-v"$su2_version"-linux64-mpi SU2-v"$su2_version"-linux64-mpi.zip
+wget https://github.com/su2code/SU2/releases/download/v"$su2_version"/SU2-v"$su2_version"-linux64.zip
+unzip -d SU2-v"$su2_version"-linux64 SU2-v"$su2_version"-linux64.zip
 
-su2_run_path=/"$install_dir"/SU2-v"$su2_version"-linux64-mpi/bin
-su2_home_path=/"$install_dir"/SU2-v"$su2_version"-linux64-mpi
+su2_run_path=/"$install_dir"/SU2-v"$su2_version"-linux64/bin
+su2_home_path=/"$install_dir"/SU2-v"$su2_version"-linux64
 
 # Export for current session
 export SU2_RUN="$su2_run_path"
@@ -41,8 +32,5 @@ export PATH="$PATH:$SU2_RUN"
 
 echo "Checking SU2 version"
 "$SU2_RUN/SU2_CFD" --help
-
-echo "Checking MPI version"
-mpirun --version
 
 cd "$current_dir"

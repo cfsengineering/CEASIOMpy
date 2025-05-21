732d712ef3d6d6782273c0f791ff731363f85d98
LeonDeligny <leon.deligny@icloud.com>
2025-05-21 15:45:00 +0200

    test not from source

--- installation/Ubuntu/install_su2.sh
+++ installation/Ubuntu/install_su2.sh
@@ -1,66 +1,48 @@
 #!/bin/bash
 
-# Script to build and install SU2 from source with MPI support
+# Script to install SU2
 
 su2_version="8.1.0"
+
 current_dir="$(pwd)"
 
 # Get install dir from input if it exists
 if [ $# -gt 0 ]; then
     install_dir="$1/INSTALLDIR"
 else
-    install_dir="$(pwd)/INSTALLDIR"
+    install_dir="$(pwd)/../../INSTALLDIR"
 fi
 
 echo "Creating install directory..."
 mkdir -p "$install_dir"
 cd "$install_dir"
 
-echo "Installing build dependencies..."
-sudo apt-get update && sudo apt-get install -y --no-install-recommends \
-    mpich libmpich-dev python3 python3-pip meson ninja-build pkg-config \
-    libhwloc-dev libpmix-dev libucx-dev
+echo "Installing Open MPI"
+apt-get update    apt-get install -y --no-install-recommends     build-essential     openmpi-bin     openmpi-doc     libopenmpi-dev &&     rm -rf /var/lib/apt/lists/*
 
-git clone --recursive --branch v${su2_version} https://github.com/su2code/SU2.git su2_source
-cd su2_source
+echo "Adding MPICH path to the .bashrc"
 
-export INSTALL_DIR="$install_dir"
-export CC=mpicc
-export CXX=mpicxx
+mpich_path="/usr/bin"
+echo export PATH=\"\$PATH:$mpich_path\" >> ~/.bashrc
+source ~/.bashrc
 
-echo "Checking MPI compiler..."
-which mpicc && mpicc --version
+echo "Downloading SU2..."
+wget https://github.com/su2code/SU2/releases/download/v"$su2_version"/SU2-v"$su2_version"-linux64-mpi.zip
+unzip -d SU2-v"$su2_version"-linux64-mpi SU2-v"$su2_version"-linux64-mpi.zip
 
-echo "Preconfiguring SU2..."
-python3 preconfigure.py
+echo "Adding path to the .bashrc"
 
-echo "Configuring SU2 with Meson..."
-python3 meson.py build --prefix="${INSTALL_DIR}" \
-    -Denable-autodiff=true \
-    -Denable-directdiff=true \
-    -Dcustom-mpi=true \
-    -Dextra-deps=mpich \
-    -Dwith-mpi=enabled \
-    -Dwith-omp=true \
-    --buildtype=release
+su2_run_path=/"$install_dir"/SU2-v"$su2_version"-linux64-mpi/bin
+su2_home_path=/"$install_dir"/SU2-v"$su2_version"-linux64-mpi
 
-# python3 meson.py build --prefix="${INSTALL_DIR}" -Dcustom-mpi=true -Dextra-deps=mpich -Dwith-mpi=enabled -Dwith-omp=true
-
-echo "Building and installing SU2..."
-ninja -C build install
-
-echo "Adding SU2 to PATH in .bashrc"
-su2_run_path="${INSTALL_DIR}/bin"
-su2_home_path="${INSTALL_DIR}"
-
-echo "# SU2 Path" >> ~/.bashrc
-echo export SU2_RUN=\"${su2_run_path}\" >> ~/.bashrc
-echo export SU2_HOME=\"${su2_home_path}\" >> ~/.bashrc
+echo \# SU2 Path >> ~/.bashrc
+echo export SU2_RUN=\""$su2_run_path"\" >> ~/.bashrc
+echo export SU2_HOME=\""$su2_home_path"\" >> ~/.bashrc
 echo export PYTHONPATH=\$PYTHONPATH:\$SU2_RUN >> ~/.bashrc
 echo export PATH=\"\$PATH:\$SU2_RUN\" >> ~/.bashrc
 
 echo "Checking SU2 version"
-"${su2_run_path}/SU2_CFD" --help
+"$SU2_RUN/SU2_CFD" --help
 
 echo "Checking MPI version"
 mpirun --version
---
+++ installation/Ubuntu/install_su2_from_source.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Script to build and install SU2 from source with MPI support
+
+su2_version="8.1.0"
+current_dir="$(pwd)"
+
+# Get install dir from input if it exists
+if [ $# -gt 0 ]; then
+    install_dir="$1/INSTALLDIR"
+else
+    install_dir="$(pwd)/INSTALLDIR"
+fi
+
+echo "Creating install directory..."
+mkdir -p "$install_dir"
+cd "$install_dir"
+
+echo "Installing build dependencies..."
+sudo apt-get update && sudo apt-get install -y --no-install-recommends \
+    mpich libmpich-dev python3 python3-pip meson ninja-build pkg-config \
+    libhwloc-dev libpmix-dev libucx-dev
+
+git clone --recursive --branch v${su2_version} https://github.com/su2code/SU2.git su2_source
+cd su2_source
+
+export INSTALL_DIR="$install_dir"
+export CC=mpicc
+export CXX=mpicxx
+
+echo "Checking MPI compiler..."
+which mpicc && mpicc --version
+
+echo "Preconfiguring SU2..."
+python3 preconfigure.py
+
+echo "Configuring SU2 with Meson..."
+python3 meson.py build --prefix="${INSTALL_DIR}" \
+    -Denable-autodiff=true \
+    -Denable-directdiff=true \
+    -Dcustom-mpi=true \
+    -Dextra-deps=mpich \
+    -Dwith-mpi=enabled \
+    -Dwith-omp=true \
+    --buildtype=release
+
+# python3 meson.py build --prefix="${INSTALL_DIR}" -Dcustom-mpi=true -Dextra-deps=mpich -Dwith-mpi=enabled -Dwith-omp=true
+
+echo "Building and installing SU2..."
+ninja -C build install
+
+echo "Adding SU2 to PATH in .bashrc"
+su2_run_path="${INSTALL_DIR}/bin"
+su2_home_path="${INSTALL_DIR}"
+
+echo "# SU2 Path" >> ~/.bashrc
+echo export SU2_RUN=\"${su2_run_path}\" >> ~/.bashrc
+echo export SU2_HOME=\"${su2_home_path}\" >> ~/.bashrc
+echo export PYTHONPATH=\$PYTHONPATH:\$SU2_RUN >> ~/.bashrc
+echo export PATH=\"\$PATH:\$SU2_RUN\" >> ~/.bashrc
+
+echo "Checking SU2 version"
+"${su2_run_path}/SU2_CFD" --help
+
+echo "Checking MPI version"
+mpirun --version
+
+cd "$current_dir"

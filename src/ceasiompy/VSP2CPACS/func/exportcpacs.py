6aae726d51f13257cd72304ab03d71f1ff13ea5d
Deligny <leon.deligny@icloud.com>
2025-12-20 18:03:12 +0000

    codacy

--- src/ceasiompy/VSP2CPACS/func/exportcpacs.py
+++ src/ceasiompy/VSP2CPACS/func/exportcpacs.py
@@ -20,14 +20,8 @@ from pathlib import Path
 
 from ceasiompy import log
 
-try:
-    from defusedxml import defuse_stdlib, minidom as _std_minidom  # type: ignore
-    defuse_stdlib()
-except ImportError:  # pragma: no cover - fallback if defusedxml missing
-    defuse_stdlib = None
-    from xml.dom import minidom as _std_minidom
-
-_document_factory = _std_minidom.Document
+from defusedxml import minidom as _std_minidom  # type: ignore
+_document_factory = _std_minidom.Document  # type: ignore
 
 
 # Functions

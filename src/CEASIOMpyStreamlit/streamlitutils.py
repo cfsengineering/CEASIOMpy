899b6e8c11a5d9aef14a19a3c959263a9d89647e
LeonDeligny <leon.deligny@icloud.com>
2026-01-26 16:49:01 +0100

    smallest available width for sidebar

--- src/CEASIOMpyStreamlit/streamlitutils.py
+++ src/CEASIOMpyStreamlit/streamlitutils.py
@@ -141,6 +141,17 @@ def create_sidebar(how_to_text, page_title="CEASIOMpy"):
 
     im = Image.open(CEASIOMPY_LOGO_PATH)
     st.set_page_config(page_title=page_title, page_icon=im)
+    st.markdown(
+        """
+        <style>
+        section[data-testid="stSidebar"] {
+            min-width: 220px;
+            width: 220px;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
     st.sidebar.image(im)
     st.sidebar.markdown(how_to_text)
 

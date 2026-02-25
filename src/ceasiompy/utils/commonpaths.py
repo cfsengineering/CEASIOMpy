db3e7dd39adf440b72eea67216fbc9f5da5943dc
LeonDeligny <leon.deligny@icloud.com>
2026-02-25 15:47:37 +0100

    add vsp2cpacs tests

--- src/ceasiompy/utils/commonpaths.py
+++ src/ceasiompy/utils/commonpaths.py
@@ -51,6 +51,9 @@ STREAMLIT_PATH = Path(SRC_PATH, "app")
 # /CEASIOMpy/test_cases/
 TEST_CASES_PATH = Path(CEASIOMPY_PATH, "test_cases")
 
+# /CEASIOMpy/geometries/vspfiles/
+VSP_DIR = CEASIOMPY_PATH / "geometries" / "vspfiles"
+
 # /CEASIOMpy/geometries/cpacsfiles/
 CPACS_FILES_PATH = Path(CEASIOMPY_PATH, "geometries", "cpacsfiles")
 
---
+++ src/ceasiompy/vsp2cpacs/tests/test_vsp2cpacs.py
@@ -0,0 +1,18 @@
+# Imports
+import pytest
+
+from ceasiompy.vsp2cpacs.vsp2cpacs import main
+
+from ceasiompy.utils.commonpaths import (
+    VSP_DIR,
+    WKDIR_PATH,
+)
+
+# Constants
+VSP_FILES = sorted(VSP_DIR.glob("*.vsp3"))
+
+
+@pytest.mark.parametrize("vsp_file", VSP_FILES, ids=[p.name for p in VSP_FILES])
+def test_convert_all_vsp_geometries_to_cpacs(vsp_file):
+    WKDIR_PATH.mkdir(exist_ok=True)
+    main(str(vsp_file))
